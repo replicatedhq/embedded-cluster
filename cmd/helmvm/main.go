e208a36373c7047b67ea28d3dd1b4223c78adb2b
danj-replicated <danj@replicated.com>
2023-09-01 14:50:21 +0100

    set global loglevel

--- cmd/helmvm/main.go
+++ cmd/helmvm/main.go
@@ -10,9 +10,28 @@ import (
 )
 
 func main() {
+
+	logrus.SetLevel(logrus.WarnLevel)
+
 	name := path.Base(os.Args[0])
 	var app = &cli.App{
-		Name:  name,
+		Name: name,
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "verbosity",
+				Usage:   "set verbosity: (warn,info,debug,error)",
+				Aliases: []string{"v"},
+				Value:   "warn",
+				Action: func(ctx *cli.Context, v string) error {
+					logLevel, err := logrus.ParseLevel(v)
+					if err != nil {
+						logrus.Fatal(err)
+					}
+					logrus.SetLevel(logLevel)
+					return nil
+				},
+			},
+		},
 		Usage: fmt.Sprintf("Installs or updates %s.", name),
 		Commands: []*cli.Command{
 			installCommand,
