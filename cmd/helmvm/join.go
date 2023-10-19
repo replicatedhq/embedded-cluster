c17cf247f7c906aeddf140d90bb6831035040caf
Ricardo Maraschini <ricardo.maraschini@gmail.com>
2023-10-19 11:52:24 -0600

    bug: only disable konnectivity server on controllers

--- cmd/helmvm/join.go
+++ cmd/helmvm/join.go
@@ -221,13 +221,10 @@ func createSystemdUnitFile(fullcmd string) error {
 // adm api.
 func runK0sInstallCommand(fullcmd string) error {
 	args := strings.Split(fullcmd, " ")
-	args = append(
-		args,
-		"--disable-components",
-		"konnectivity-server",
-		"--token-file",
-		"/etc/k0s/join-token",
-	)
+	args = append(args, "--token-file", "/etc/k0s/join-token")
+	if strings.Contains(fullcmd, "controller") {
+		args = append(args, "--disable-components", "konnectivity-server")
+	}
 	cmd := exec.Command(args[0], args[1:]...)
 	stdout := bytes.NewBuffer(nil)
 	stderr := bytes.NewBuffer(nil)
