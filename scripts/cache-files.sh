dcc2a132ab36270b08656ef718b438fcd68718cc
Andrew Lavery <laverya@umich.edu>
2024-01-04 16:50:07 -0500

    ?

--- scripts/cache-files.sh
+++ scripts/cache-files.sh
@@ -54,14 +54,15 @@ function k0sbin() {
     # if the override is set, the binary will have been added to the bucket through another process
     if [ -n "${k0s_override}" ] && [ "${k0s_override}" != "" ]; then
         echo "K0S_BINARY_SOURCE_OVERRIDE is set to '${k0s_override}', using that source"
+        echo "Curl command: curl -L -o ${k0s_version} ${k0s_override}"
         curl -L -o "${k0s_version}" "${k0s_override}"
     else
         # download the k0s binary from official sources
-        curl -L -o "$(k0s_version)" "https://github.com/k0sproject/k0s/releases/download/$(k0s_version)/k0s-$(k0s_version)-amd64"
+        curl -L -o "${k0s_version}" "https://github.com/k0sproject/k0s/releases/download/${k0s_version}/k0s-${k0s_version}-amd64"
     fi
 
     # upload the binary to the bucket
-    retry 3 aws s3 cp "$(k0s_version)" "s3://${S3_BUCKET}/k0s-binaries/${k0s_version}"
+    retry 3 aws s3 cp "${k0s_version}" "s3://${S3_BUCKET}/k0s-binaries/${k0s_version}"
 }
 
 function metadata() {
