5e6fe2292cf0d58765f8c5633d1f7e8362a01446
Andrew Lavery <laverya@umich.edu>
2024-01-29 16:45:13 -0500

    testing

--- e2e/scripts/check-installation-state.sh
+++ e2e/scripts/check-installation-state.sh
@@ -2,12 +2,15 @@
 set -euo pipefail
 
 main() {
+    sleep 60
     echo "installations"
     kubectl get installations
     kubectl describe installations
+    kubectl get installations -o yaml
     echo "charts"
     kubectl get charts -A
     kubectl describe charts -A
+    kubectl get charts -A -o yaml
 }
 
 export EMBEDDED_CLUSTER_METRICS_BASEURL="https://staging.replicated.app"
