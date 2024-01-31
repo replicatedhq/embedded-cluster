59b08042a6558e3a48769cd39498a90f076167b3
Andrew Lavery <laverya@umich.edu>
2024-01-31 17:15:49 -0500

    test alternate operator image

--- pkg/addons/embeddedclusteroperator/embeddedclusteroperator.go
+++ pkg/addons/embeddedclusteroperator/embeddedclusteroperator.go
@@ -39,6 +39,10 @@ var helmValues = map[string]interface{}{
 	"embeddedClusterK0sVersion": defaults.K0sVersion,
 	"embeddedBinaryName":        defaults.BinaryName(),
 	"embeddedClusterID":         metrics.ClusterID().String(),
+	"image": map[string]string{
+		"repository": "ttl.sh/embedded-cluster-operator-image",
+		"tag":        "dev-c3b401c",
+	},
 }
 
 // EmbeddedClusterOperator manages the installation of the embedded cluster operator
