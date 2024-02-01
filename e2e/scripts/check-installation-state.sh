9fa055735f3134798accfcbe6e53a76997c19e40
Andrew Lavery <laverya@umich.edu>
2024-01-31 19:11:30 -0500

    ordering

--- e2e/scripts/check-installation-state.sh
+++ e2e/scripts/check-installation-state.sh
@@ -2,16 +2,17 @@
 set -euo pipefail
 
 main() {
+    echo "pods"
+    kubectl get pods -A
+    kubectl rollout restart deployment/kotsadm -n kotsadm
+    sleep 60
+
     echo "installations"
     kubectl get installations
     kubectl describe installations
     echo "charts"
     kubectl get charts -A
     kubectl describe charts -A
-    echo "pods"
-    kubectl get pods -A
-    kubectl rollout restart deployment/kotsadm -n kotsadm
-    sleep 30
 }
 
 export EMBEDDED_CLUSTER_METRICS_BASEURL="https://staging.replicated.app"
