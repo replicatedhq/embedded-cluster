9c927f67b4569cf3d8a901fd75a66a0d0bba79a1
Andrew Lavery <laverya@umich.edu>
2024-02-06 08:37:36 -0500

    fix puppeteer tests (#325)
    * skip visiting new tab page
    
    * wasn't it
    
    * pin previous puppeteer

--- e2e/scripts/install-puppeteer.sh
+++ e2e/scripts/install-puppeteer.sh
@@ -44,4 +44,4 @@ curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dea
 NODE_MAJOR=20
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
 apt-get update && apt-get install nodejs -y
-npm install -g puppeteer
+npm install -g puppeteer@v21.11.0
