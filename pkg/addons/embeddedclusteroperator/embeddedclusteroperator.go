36751462a73c1baebd0a522bed6b2dc85901a659
Andrew Lavery <laverya@umich.edu>
2024-02-08 18:48:10 -0500

    operator b6c2e1d

--- pkg/addons/embeddedclusteroperator/embeddedclusteroperator.go
+++ pkg/addons/embeddedclusteroperator/embeddedclusteroperator.go
@@ -41,7 +41,7 @@ var helmValues = map[string]interface{}{
 	"embeddedClusterID":         metrics.ClusterID().String(),
 	"image": map[string]interface{}{
 		"repository": "ttl.sh/embedded-cluster-operator-image",
-		"tag":        "dev-b4e9ec3",
+		"tag":        "dev-b6c2e1d",
 	},
 }
 
