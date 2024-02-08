59f0114326ae2c0613ec83e9c6e504beb30e4b14
Andrew Lavery <laverya@umich.edu>
2024-02-08 17:48:31 -0500

    updated operator image

--- pkg/addons/embeddedclusteroperator/embeddedclusteroperator.go
+++ pkg/addons/embeddedclusteroperator/embeddedclusteroperator.go
@@ -41,7 +41,7 @@ var helmValues = map[string]interface{}{
 	"embeddedClusterID":         metrics.ClusterID().String(),
 	"image": map[string]interface{}{
 		"repository": "ttl.sh/embedded-cluster-operator-image",
-		"tag":        "dev-ae48d9d",
+		"tag":        "dev-98e3f68",
 	},
 }
 
