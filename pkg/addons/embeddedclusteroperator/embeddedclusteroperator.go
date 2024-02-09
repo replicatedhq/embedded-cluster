dc66a4661b6fb86101f97388d1b488d961bab521
Andrew Lavery <laverya@umich.edu>
2024-02-09 08:56:18 -0500

    official embedded-cluster release

--- pkg/addons/embeddedclusteroperator/embeddedclusteroperator.go
+++ pkg/addons/embeddedclusteroperator/embeddedclusteroperator.go
@@ -39,10 +39,6 @@ var helmValues = map[string]interface{}{
 	"embeddedClusterK0sVersion": defaults.K0sVersion,
 	"embeddedBinaryName":        defaults.BinaryName(),
 	"embeddedClusterID":         metrics.ClusterID().String(),
-	"image": map[string]interface{}{
-		"repository": "ttl.sh/embedded-cluster-operator-image",
-		"tag":        "dev-e04f805",
-	},
 }
 
 // EmbeddedClusterOperator manages the installation of the embedded cluster operator
