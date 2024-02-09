99c72120350a88b191afc9f63fddfd40cdebe48d
Andrew Lavery <laverya@umich.edu>
2024-02-08 20:17:39 -0500

    update postupgrade test

--- e2e/scripts/check-postupgrade-state.sh
+++ e2e/scripts/check-postupgrade-state.sh
@@ -64,10 +64,8 @@ main() {
 
     # ensure that nginx-ingress has been updated
     kubectl describe chart -n kube-system k0s-addon-chart-ingress-nginx
-    kubectl describe pods -n ingress-nginx
-    kubectl get clusterconfig -n kube-system k0s -o yaml
-    kubectl get clusterconfig -n kube-system k0s -o yaml | grep -q "test-upgrade-value"
-    kubectl describe pods -n ingress-nginx | grep -q "test-upgrade-value"
+    kubectl describe chart -n kube-system k0s-addon-chart-ingress-nginx | grep -q "test-upgrade-value" # ensure new values are present
+    kubectl describe chart -n kube-system k0s-addon-chart-ingress-nginx | grep -q "1.9.1" # ensure new version is present
 }
 
 export EMBEDDED_CLUSTER_METRICS_BASEURL="https://staging.replicated.app"
