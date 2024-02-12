1d20b54df2bf6ac76c2ea4277880b6b63b39ef04
Andrew Lavery <laverya@umich.edu>
2024-02-12 18:59:50 -0500

    create dedicated script to upload the upgrade metadata

---
+++ scripts/create-upgrade-release.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -eo pipefail
+
+function require() {
+    if [ -z "$2" ]; then
+        echo "validation failed: $1 unset"
+        exit 1
+    fi
+}
+
+require AWS_ACCESS_KEY_ID "${AWS_ACCESS_KEY_ID}"
+require AWS_SECRET_ACCESS_KEY "${AWS_SECRET_ACCESS_KEY}"
+require AWS_REGION "${AWS_REGION}"
+require S3_BUCKET "${S3_BUCKET}"
+
+function retry() {
+    local retries=$1
+    shift
+
+    local count=0
+    until "$@"; do
+        exit=$?
+        wait=$((2 ** $count))
+        count=$(($count + 1))
+        if [ $count -lt $retries ]; then
+            echo "Retry $count/$retries exited $exit, retrying in $wait seconds..."
+            sleep $wait
+        else
+            echo "Retry $count/$retries exited $exit, no more retries left."
+            return $exit
+        fi
+    done
+    return 0
+}
+
+function metadata() {
+    if [ -z "${EC_VERSION}" ]; then
+        echo "EC_VERSION unset, not uploading metadata.json"
+        return 0
+    fi
+
+    # check if a file 'metadata.json' exists in the directory
+    # if it does, upload it as metadata/${ec_version}.json
+    if [ -f metadata.json ]; then
+        retry 3 aws s3 cp metadata.json "s3://${S3_BUCKET}/metadata/${EC_VERSION}.json"
+    else
+        echo "metadata.json not found, skipping upload"
+    fi
+
+}
+
+function main() {
+    export EC_VERSION="${EC_VERSION}-upgrade"
+    metadata
+}
+
+main "$@"
