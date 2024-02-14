5acac55d37edc4cb75538940639f8e4dc667f84b
Andrew Lavery <laverya@umich.edu>
2024-02-14 11:12:17 -0500

    feat: remove node start, stop, list commands (#357)
    remove node start, stop, list commands
    
    also fix running 'node join help'/'node reset help' as non-root

--- cmd/embedded-cluster/node.go
+++ cmd/embedded-cluster/node.go
@@ -1,82 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"os/exec"
-
 	"github.com/urfave/cli/v2"
-
-	"github.com/replicatedhq/embedded-cluster/pkg/defaults"
 )
 
 var nodeCommands = &cli.Command{
 	Name:  "node",
 	Usage: "Manage cluster nodes",
-	Before: func(c *cli.Context) error {
-		if os.Getuid() != 0 {
-			return fmt.Errorf("node command must be run as root")
-		}
-		return nil
-	},
 	Subcommands: []*cli.Command{
-		nodeStopCommand,
-		nodeStartCommand,
-		nodeListCommand,
 		joinCommand,
 		resetCommand,
-	},
-}
-
-var nodeStopCommand = &cli.Command{
-	Name:  "stop",
-	Usage: "Stops a node",
-	Action: func(c *cli.Context) error {
-		node := c.Args().First()
-		if node == "" {
-			return fmt.Errorf("expected node name")
-		}
-		kcfg := defaults.PathToConfig("kubeconfig")
-		os.Setenv("KUBECONFIG", kcfg)
-		bin := defaults.PathToEmbeddedClusterBinary("kubectl")
-		cmd := exec.Command(bin, "drain", "--ignore-daemonsets", node)
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		cmd.Stdin = os.Stdin
-		return cmd.Run()
-	},
-}
-
-var nodeStartCommand = &cli.Command{
-	Name:  "start",
-	Usage: "Starts a node",
-	Action: func(c *cli.Context) error {
-		node := c.Args().First()
-		if node == "" {
-			return fmt.Errorf("expected node name")
-		}
-		kcfg := defaults.PathToConfig("kubeconfig")
-		os.Setenv("KUBECONFIG", kcfg)
-		bin := defaults.PathToEmbeddedClusterBinary("kubectl")
-		cmd := exec.Command(bin, "uncordon", node)
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		cmd.Stdin = os.Stdin
-		return cmd.Run()
-	},
-}
-
-var nodeListCommand = &cli.Command{
-	Name:  "list",
-	Usage: "List all nodes",
-	Action: func(c *cli.Context) error {
-		kcfg := defaults.PathToConfig("kubeconfig")
-		os.Setenv("KUBECONFIG", kcfg)
-		bin := defaults.PathToEmbeddedClusterBinary("kubectl")
-		cmd := exec.Command(bin, "get", "nodes", "-o", "wide")
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		cmd.Stdin = os.Stdin
-		return cmd.Run()
 	},
 }
