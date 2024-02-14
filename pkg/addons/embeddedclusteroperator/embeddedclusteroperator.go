f4acf7f37041b98c68281c37cbe794f842a4b0b7
Andrew Lavery <laverya@umich.edu>
2024-02-14 18:26:37 -0500

    resolve chart values so that charts are not immediately reapplied

--- pkg/addons/embeddedclusteroperator/embeddedclusteroperator.go
+++ pkg/addons/embeddedclusteroperator/embeddedclusteroperator.go
@@ -36,7 +36,6 @@ var (
 
 var helmValues = map[string]interface{}{
 	"kotsVersion":               adminconsole.Version,
-	"embeddedClusterVersion":    defaults.Version,
 	"embeddedClusterK0sVersion": defaults.K0sVersion,
 	"embeddedBinaryName":        defaults.BinaryName(),
 	"embeddedClusterID":         metrics.ClusterID().String(),
