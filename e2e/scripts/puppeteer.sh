d532f85eae4fa92cdd08323a3b43ef1bb2e9f1a5
Andrew Lavery <laverya@umich.edu>
2024-02-14 22:10:37 -0500

    remove test logging

--- e2e/scripts/puppeteer.sh
+++ e2e/scripts/puppeteer.sh
@@ -1,12 +1,3 @@
 #!/usr/bin/env bash
 set -euo pipefail
-if ! NODE_PATH="$(npm root -g)" "$@"; then
-    export EMBEDDED_CLUSTER_METRICS_BASEURL="https://staging.replicated.app"
-    export KUBECONFIG=/root/.config/embedded-cluster/etc/kubeconfig
-    export PATH=$PATH:/root/.config/embedded-cluster/bin
-    sleep 30
-    echo "pods"
-    kubectl get pods -A
-    echo "password secret"
-    kubectl get secret -n kotsadm kotsadm-password -o yaml
-fi
+NODE_PATH="$(npm root -g)" "$@"
