b13bc9abd64b03e9e6332714869e4eaaeb590f6c
Andrew Lavery <laverya@umich.edu>
2024-02-15 19:02:55 -0500

    use license from github secret (#375)

--- e2e/scripts/vandoor-prepare.sh
+++ e2e/scripts/vandoor-prepare.sh
@@ -5,11 +5,13 @@ set -euo pipefail
 main() {
     local sha=
     sha="$1"
+    local license_id=
+    license_id="$2"
 
     apt-get update
     apt-get install curl ca-certificates -y
 
-    curl "https://staging.replicated.app/embedded/embedded-cluster-smoke-test-staging-app/ci/${sha}" -H 'Authorization: 2cHePFvlGDksGOKfTicouqJKGtZ' -o ec-release.tgz
+    curl "https://staging.replicated.app/embedded/embedded-cluster-smoke-test-staging-app/ci/${sha}" -H "Authorization: ${license_id}" -o ec-release.tgz
     tar xzf ec-release.tgz
 
     mv embedded-cluster-smoke-test-staging-app /usr/local/bin/embedded-cluster
