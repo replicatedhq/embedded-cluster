0fd683fd717be93b7ae5de36b2dbeaf4b2a3c4ed
Andrew Lavery <laverya@umich.edu>
2024-02-15 23:59:53 -0500

    undo reset change

--- cmd/embedded-cluster/reset.go
+++ cmd/embedded-cluster/reset.go
@@ -87,7 +87,6 @@ func (h *hostInfo) deleteControlNode(ctx context.Context) error {
 func (h *hostInfo) drainNode() error {
 	os.Setenv("KUBECONFIG", h.Status.Vars.KubeletAuthConfigPath)
 	drainArgList := []string{
-		"/usr/local/bin/k0s",
 		"kubectl",
 		"drain",
 		"--ignore-daemonsets",
