00e2573620eef2560978ab3d9599574529241388
Andrew Lavery <laverya@umich.edu>
2024-02-22 17:12:31 -0500

    count goldpinger pods, not running goldpinger pods

--- e2e/scripts/wait-for-ready-nodes.sh
+++ e2e/scripts/wait-for-ready-nodes.sh
@@ -22,7 +22,7 @@ main() {
     echo "checking that goldpinger has run on all nodes"
     kubectl get pods -n goldpinger
     local goldpinger_running_count=
-    goldpinger_running_count=$(kubectl get pods -n goldpinger -o jsonpath='{.items[*].status.phase}' | grep -c Running || true)
+    goldpinger_running_count=$(kubectl get pods -n goldpinger -o jsonpath='{.items[*].status.phase}' | wc -l || true)
     if [ "$goldpinger_running_count" -lt "$expected_nodes" ]; then
         echo "goldpinger is running on $goldpinger_running_count nodes, expected $expected_nodes"
         exit 1
