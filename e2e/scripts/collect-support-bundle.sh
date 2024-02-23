daf9482b8ef68fb5614255fc4c8daf26297a6409
Ricardo Maraschini <ricardo.maraschini@gmail.com>
2024-02-23 12:44:40 +0100

    feat: check if support bundle contains expected files (#413)
    check if we have some expected files and directories inside the
    generated support bundle.

--- e2e/scripts/collect-support-bundle.sh
+++ e2e/scripts/collect-support-bundle.sh
@@ -7,8 +7,20 @@ main() {
         return 1
     fi
 
+    tar -zxvf host.tar.gz
+    if ! ls host/host-collectors/run-host/k0s-sysinfo.txt; then
+        echo "Failed to find 'k0s sysinfo' inside the host support bundle"
+        return 1
+    fi
+
     if ! kubectl support-bundle --output cluster.tar.gz --interactive=false --load-cluster-specs; then
         echo "Failed to collect cluster support bundle"
+        return 1
+    fi
+
+    tar -zxvf cluster.tar.gz
+    if ! ls cluster/podlogs/embedded-cluster-operator; then
+        echo "Failed to find operator logs inside the cluster support bundle"
         return 1
     fi
 }
