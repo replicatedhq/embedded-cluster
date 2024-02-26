69ac73ad62f82f539d23c08c41f5a7dacd8f0731
Andrew Lavery <laverya@umich.edu>
2024-02-26 14:53:06 -0600

    check internet with sh

--- e2e/cluster/cluster.go
+++ e2e/cluster/cluster.go
@@ -25,7 +25,7 @@ lxc.cap.drop=
 lxc.cgroup.devices.allow=a
 lxc.mount.auto=proc:rw sys:rw
 lxc.mount.entry = /dev/kmsg dev/kmsg none defaults,bind,create=file`
-const checkInternet = `#!/bin/bash
+const checkInternet = `#!/bin/sh
 timeout 5 bash -c 'cat < /dev/null > /dev/tcp/www.replicated.com/80'
 if [ $? == 0 ]; then
     exit 0
