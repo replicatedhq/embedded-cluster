c0c3206417ad0306c7f60c89643396e5b01a6a04
Andrew Lavery <laverya@umich.edu>
2024-03-06 16:31:30 -0500

    set sha properly

--- scripts/create-upgrade-release.sh
+++ scripts/create-upgrade-release.sh
@@ -45,7 +45,8 @@ function metadata() {
         sudo apt-get install jq -y
 
         jq '(.Configs.charts[] | select(.name == "embedded-cluster-operator")).values += "global:\n  labels:\n    embedded-cluster-operator-upgrade-label: embedded-cluster-operator-upgrade-value"' metadata.json > temp.json
-        jq '(.versions.kubernetes = "v1.29.2+k0s.0")' temp.json > upgrade-metadata.json
+        jq '(.versions.kubernetes = "v1.29.2+k0s.0")' temp.json > temp2.json
+        jq '(.K0sSHA = "298cc05b1b4521006f470eccf6c5e780e5601b60c97eb259732b622be922ceee")' temp2.json > upgrade-metadata.json
         cat upgrade-metadata.json
 
         retry 3 aws s3 cp upgrade-metadata.json "s3://${S3_BUCKET}/metadata/${EC_VERSION}.json"
