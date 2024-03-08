97d2dad18d7206483bab96a5d90c3f1c3ee4447f
Andrew Lavery <laverya@umich.edu>
2024-03-08 13:08:54 -0500

    f

--- cmd/embedded-cluster/version.go
+++ cmd/embedded-cluster/version.go
@@ -99,7 +99,7 @@ var metadataCommand = &cli.Command{
 		meta.Protected = protectedFields
 
 		// Render k0s config to get the images contained within
-		k0sConfig, err := config.RenderK0sConfig(c.Context)
+		k0sConfig := k0sconfig.DefaultClusterConfig()
 		if err != nil {
 			return fmt.Errorf("unable to render k0s config: %w", err)
 		}
