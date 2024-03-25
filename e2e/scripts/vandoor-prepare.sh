afc88865f863849f472c6cbaf375abded3b6de65
Andrew Lavery <laverya@umich.edu>
2024-03-25 23:37:37 +0900

    wait for airgap build to complete

--- e2e/scripts/vandoor-prepare.sh
+++ e2e/scripts/vandoor-prepare.sh
@@ -1,11 +1,32 @@
 #!/usr/bin/env bash
 set -euox pipefail
 
+move_airgap() {
+    # if an airgap file exists with pattern *.airgap, move it to /tmp/release.airgap
+    for file in *.airgap;
+    do
+      if [ -e "$file" ]
+      then
+        mv "$file" /tmp/release.airgap
+        break
+      fi
+    done
+
+    # if there is no file at /tmp/release.airgap, this is an error
+    if [ ! -e /tmp/release.airgap ]
+    then
+      echo "No airgap file found"
+      exit 1
+    fi
+}
+
 main() {
     local app_version_label=
     app_version_label="$1"
     local license_id=
     license_id="$2"
+    local is_airgap=
+    is_airgap="$3"
 
     apt-get update
     apt-get install curl ca-certificates -y
@@ -16,15 +37,9 @@ main() {
     mv embedded-cluster-smoke-test-staging-app /usr/local/bin/embedded-cluster
     mv license.yaml /tmp/license.yaml
 
-    # if an airgap file exists with pattern *.airgap, move it to /tmp/release.airgap
-    for file in *.airgap;
-    do
-      if [ -e "$file" ]
-      then
-        mv "$file" /tmp/release.airgap
-        break
-      fi
-    done
+    if [ "$is_airgap" = "true" ]; then
+        move_airgap
+    fi
 }
 
 main "$@"
