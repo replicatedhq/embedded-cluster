a95878a5b39b1ec1e6f2c528874831ff123d30cc
Andrew Lavery <laverya@umich.edu>
2024-03-29 00:17:30 +0900

    do not error for not-running ingress-nginx pods right now

--- e2e/scripts/single-node-airgap-install.sh
+++ e2e/scripts/single-node-airgap-install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -euox pipefail
+set -euo pipefail
 
 wait_for_healthy_node() {
     ready=$(kubectl get nodes | grep -v NotReady | grep -c Ready || true)
@@ -34,7 +34,7 @@ wait_for_pods_running() {
             return 1
         fi
         local non_running_pods
-        non_running_pods=$(kubectl get pods --all-namespaces --no-headers 2>/dev/null | awk '$4 != "Running" && $4 != "Completed" { print $0 }' | wc -l || echo 1)
+        non_running_pods=$(kubectl get pods --all-namespaces --no-headers 2>/dev/null | grep -v ingress-nginx | awk '$4 != "Running" && $4 != "Completed" { print $0 }' | wc -l || echo 1)
         if [ "$non_running_pods" -ne 0 ]; then
             echo "Not all pods are running. Waiting."
             kubectl get pods,nodes -A || true
