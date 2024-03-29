0191eb91b1f1d1a65c88af8e36039279f7b0cb76
Andrew Lavery <laverya@umich.edu>
2024-03-29 22:50:54 +0900

    remove debug logging

--- e2e/scripts/single-node-airgap-install.sh
+++ e2e/scripts/single-node-airgap-install.sh
@@ -143,8 +143,6 @@ wait_for_installation() {
 main() {
     if ! embedded-cluster install --no-prompt --license /tmp/license.yaml --airgap-bundle /tmp/release.airgap 2>&1 | tee /tmp/log ; then
         echo "Failed to install embedded-cluster"
-        kubectl get storageclass
-        kubectl get pvc -A
         exit 1
     fi
     if ! grep -q "Admin Console is ready!" /tmp/log; then
