65d3cc015ef5430b1fd65bb3e26e3af621677828
Craig O'Donnell <craig@replicated.com>
2024-04-01 19:20:31 +0000

    wip: replace puppeteer with testim

--- e2e/reset_test.go
+++ e2e/reset_test.go
@@ -1,7 +1,7 @@
 package e2e
 
 import (
-	"encoding/json"
+	"os"
 	"strings"
 	"testing"
 	"time"
@@ -28,39 +28,35 @@ func TestMultiNodeReset(t *testing.T) {
 		t.Fatalf("fail to install embedded-cluster on node %s: %v", tc.Nodes[0], err)
 	}
 
-	runPuppeteerAppStatusCheck(t, 0, tc)
+	installTestimAndDeploy(t, 0, tc)
 
 	// generate all node join commands (2 for controllers and 1 for worker).
 	t.Logf("%s: generating two new controller token commands", time.Now().Format(time.RFC3339))
 	controllerCommands := []string{}
 	for i := 0; i < 2; i++ {
-		line := []string{"puppeteer.sh", "generate-controller-join-token.js", "10.0.0.2"}
+		line := []string{"testim.sh", os.Getenv("TESTIM_ACCESS_TOKEN"), os.Getenv("TESTIM_BRANCH"), "embedded-cluster-join-controller-command"}
 		stdout, stderr, err := RunCommandOnNode(t, tc, 0, line)
 		if err != nil {
-			t.Fatalf("fail to generate controller join token: %s", stdout)
+			t.Fatalf("fail to generate controller join token:\nstdout: %s\nstderr: %s", stdout, stderr)
 		}
-		var r nodeJoinResponse
-		if err := json.Unmarshal([]byte(stdout), &r); err != nil {
-			t.Logf("stdout: %s\nstderr: %s", stdout, stderr)
-			t.Fatalf("fail to parse script response: %v", err)
+		command, err := findJoinCommandInOutput(stdout)
+		if err != nil {
+			t.Fatalf("fail to find the join command in the output: %v", err)
 		}
-		// trim down the "./" and the "sudo" command as those are not needed. we run as
-		// root and the embedded-cluster binary is on the PATH.
-		command := strings.TrimPrefix(r.Command, "sudo ./")
 		controllerCommands = append(controllerCommands, command)
 		t.Log("controller join token command:", command)
 	}
 	t.Logf("%s: generating a new worker token command", time.Now().Format(time.RFC3339))
-	line := []string{"puppeteer.sh", "generate-worker-join-token.js", "10.0.0.2"}
+	line := []string{"testim.sh", os.Getenv("TESTIM_ACCESS_TOKEN"), os.Getenv("TESTIM_BRANCH"), "embedded-cluster-join-worker-command"}
 	stdout, stderr, err := RunCommandOnNode(t, tc, 0, line)
 	if err != nil {
-		t.Fatalf("fail to generate controller join token: %s", stdout)
+		t.Fatalf("fail to generate worker join token:\nstdout: %s\nstderr: %s", stdout, stderr)
 	}
-	var jr nodeJoinResponse
-	if err := json.Unmarshal([]byte(stdout), &jr); err != nil {
-		t.Logf("stdout: %s\nstderr: %s", stdout, stderr)
-		t.Fatalf("fail to parse script response: %v", err)
+	command, err := findJoinCommandInOutput(stdout)
+	if err != nil {
+		t.Fatalf("fail to find the join command in the output: %v", err)
 	}
+	t.Log("worker join token command:", command)
 
 	// join the nodes.
 	for i, cmd := range controllerCommands {
@@ -78,8 +74,6 @@ func TestMultiNodeReset(t *testing.T) {
 		t.Logf("node %d joined, sleeping...", node)
 		time.Sleep(30 * time.Second)
 	}
-	command := strings.TrimPrefix(jr.Command, "sudo ./")
-	t.Log("worker join token command:", command)
 	t.Logf("%s: joining node 3 to the cluster as a worker", time.Now().Format(time.RFC3339))
 	if _, _, err := RunCommandOnNode(t, tc, 3, strings.Split(command, " ")); err != nil {
 		t.Fatalf("fail to join node 3 to the cluster as a worker: %v", err)
