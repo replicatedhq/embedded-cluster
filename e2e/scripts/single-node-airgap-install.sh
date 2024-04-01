3ff99512f9d2882a3d904608f2dbf73511925e9d
Andrew Lavery <laverya@umich.edu>
2024-04-01 23:38:41 +0900

    feat: use airgapped installation metadata when relevant (#471)
    reenable 'wait for installation' in airgap test

--- e2e/scripts/single-node-airgap-install.sh
+++ e2e/scripts/single-node-airgap-install.sh
@@ -191,9 +191,9 @@ main() {
         exit 1
     fi
 
-#    echo "ensure that installation is installed"
-#    wait_for_installation
-#    kubectl get installations --no-headers | grep -q "Installed"
+    echo "ensure that installation is installed"
+    wait_for_installation
+    kubectl get installations --no-headers | grep -q "Installed"
 }
 
 export EMBEDDED_CLUSTER_METRICS_BASEURL="https://staging.replicated.app"
