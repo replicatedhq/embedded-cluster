95dcd1c36266027d2996443f72c4657d180fb06c
Andrew Lavery <laverya@umich.edu>
2024-04-04 01:45:29 +0900

    do not print debug log to screen on install failure (#480)
    * do not print debug log to screen on install failure
    
    * this log is redundant with the one produced by helpers.RunCommand

--- pkg/addons/adminconsole/adminconsole.go
+++ pkg/addons/adminconsole/adminconsole.go
@@ -293,7 +293,6 @@ func (a *AdminConsole) Outro(ctx context.Context, cli client.Client) error {
 	}
 
 	if _, err := helpers.RunCommand(kotsBinPath, installArgs...); err != nil {
-		loading.Debugf("kubectl-kots %v", installArgs)
 		loading.Close()
 		return fmt.Errorf("unable to install the application: %w", err)
 	}
