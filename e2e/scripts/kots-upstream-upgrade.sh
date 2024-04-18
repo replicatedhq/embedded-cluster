e075e7e90f28b7947b5cd264dc134b5292119b73
Craig O'Donnell <craig@replicated.com>
2024-04-18 21:13:45 +0000

    clean up scripts

--- e2e/scripts/kots-upstream-upgrade.sh
+++ e2e/scripts/kots-upstream-upgrade.sh
@@ -11,9 +11,9 @@ main() {
     else
         echo "upgrading to version ${installation_version}-upgrade from online"
         kubectl kots upstream upgrade embedded-cluster-smoke-test-staging-app --namespace kotsadm --deploy-version-label="appver-${installation_version}-upgrade"
+        sleep 30 # wait for the app version to be created
     fi
 
-    sleep 30 # wait for the app version to be created
 }
 
 export KUBECONFIG=/var/lib/k0s/pki/admin.conf
