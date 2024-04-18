9127d8138e210089f32fd8b106a3ee2126be3e66
Craig O'Donnell <craig@replicated.com>
2024-04-18 16:39:53 +0000

    just add on to the existing airgap test

--- e2e/install_test.go
+++ e2e/install_test.go
@@ -413,74 +413,6 @@ func TestSingleNodeAirgapInstallationUbuntuJammy(t *testing.T) {
 	t.Parallel()
 
 	t.Logf("%s: downloading airgap files", time.Now().Format(time.RFC3339))
-	airgapInstallBundlePath := downloadAirgapBundle(t, fmt.Sprintf("appver-%s", os.Getenv("SHORT_SHA")), "/tmp/airgap-bundle.tar.gz")
-
-	t.Logf("%s: creating airgap node", time.Now().Format(time.RFC3339))
-
-	tc := cluster.NewTestCluster(&cluster.Input{
-		T:                       t,
-		Nodes:                   1,
-		Image:                   "ubuntu/jammy",
-		WithProxy:               true,
-		AirgapInstallBundlePath: airgapInstallBundlePath,
-	})
-	defer tc.Destroy()
-
-	t.Logf("%s: preparing embedded cluster airgap files", time.Now().Format(time.RFC3339))
-	line := []string{"airgap-prepare.sh"}
-
-	if _, _, err := RunCommandOnNode(t, tc, 0, line); err != nil {
-		t.Fatalf("fail to prepare airgap files on node %s: %v", tc.Nodes[0], err)
-	}
-
-	t.Logf("%s: installing embedded-cluster on node 0", time.Now().Format(time.RFC3339))
-	line = []string{"single-node-airgap-install.sh"}
-	if _, _, err := RunCommandOnNode(t, tc, 0, line); err != nil {
-		t.Fatalf("fail to install embedded-cluster on node %s: %v", tc.Nodes[0], err)
-	}
-
-	t.Logf("%s: test complete", time.Now().Format(time.RFC3339))
-}
-
-func downloadAirgapBundle(t *testing.T, versionLabel string, destPath string) string {
-	// download airgap bundle
-	airgapURL := fmt.Sprintf("https://staging.replicated.app/embedded/embedded-cluster-smoke-test-staging-app/ci-airgap/%s?airgap=true", versionLabel)
-
-	req, err := http.NewRequest("GET", airgapURL, nil)
-	if err != nil {
-		t.Fatalf("failed to create request: %v", err)
-	}
-	req.Header.Set("Authorization", os.Getenv("AIRGAP_LICENSE_ID"))
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		t.Fatalf("failed to download airgap bundle: %v", err)
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		t.Fatalf("failed to download airgap bundle: %s", resp.Status)
-	}
-
-	// pipe response to a temporary file
-	airgapBundlePath := destPath
-	f, err := os.Create(airgapBundlePath)
-	if err != nil {
-		t.Fatalf("failed to create temporary file: %v", err)
-	}
-	defer f.Close()
-	size, err := f.ReadFrom(resp.Body)
-	if err != nil {
-		t.Fatalf("failed to write response to temporary file: %v", err)
-	}
-	t.Logf("downloaded airgap bundle to %s (%d bytes)", airgapBundlePath, size)
-
-	return airgapBundlePath
-}
-
-func TestSingleNodeAirgapUpgradeUbuntuJammy(t *testing.T) {
-	t.Parallel()
-
-	t.Logf("%s: downloading airgap files", time.Now().Format(time.RFC3339))
 	airgapInstallBundlePath := downloadAirgapBundle(t, fmt.Sprintf("appver-%s", os.Getenv("SHORT_SHA")), "/tmp/airgap-install-bundle.tar.gz")
 	airgapUpgradeBundlePath := downloadAirgapBundle(t, fmt.Sprintf("appver-%s-upgrade", os.Getenv("SHORT_SHA")), "/tmp/airgap-upgrade-bundle.tar.gz")
 
@@ -537,6 +469,41 @@ func TestSingleNodeAirgapUpgradeUbuntuJammy(t *testing.T) {
 	}
 
 	t.Logf("%s: test complete", time.Now().Format(time.RFC3339))
+}
+
+func downloadAirgapBundle(t *testing.T, versionLabel string, destPath string) string {
+	// download airgap bundle
+	airgapURL := fmt.Sprintf("https://staging.replicated.app/embedded/embedded-cluster-smoke-test-staging-app/ci-airgap/%s?airgap=true", versionLabel)
+
+	req, err := http.NewRequest("GET", airgapURL, nil)
+	if err != nil {
+		t.Fatalf("failed to create request: %v", err)
+	}
+	req.Header.Set("Authorization", os.Getenv("AIRGAP_LICENSE_ID"))
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatalf("failed to download airgap bundle: %v", err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("failed to download airgap bundle: %s", resp.Status)
+	}
+
+	// pipe response to a temporary file
+	airgapBundlePath := destPath
+	f, err := os.Create(airgapBundlePath)
+	if err != nil {
+		t.Fatalf("failed to create temporary file: %v", err)
+	}
+	defer f.Close()
+	size, err := f.ReadFrom(resp.Body)
+	if err != nil {
+		t.Fatalf("failed to write response to temporary file: %v", err)
+	}
+	t.Logf("downloaded airgap bundle to %s (%d bytes)", airgapBundlePath, size)
+
+	return airgapBundlePath
 }
 
 func setupTestim(t *testing.T, tc *cluster.Output) error {
