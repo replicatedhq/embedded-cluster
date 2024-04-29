b7c561163192dbd6881a14fb0ce091bda0a1aae8
Andrew Lavery <laverya@umich.edu>
2024-04-30 00:35:49 +0900

    use openebs-archive chart repo (#554)
    * use openebs-archive chart repo
    
    * update unsupported-overrides test

--- e2e/scripts/unsupported-overrides.sh
+++ e2e/scripts/unsupported-overrides.sh
@@ -57,7 +57,7 @@ spec:
                 version: 6.6.2
               repositories:
               - name: openebs
-                url: https://openebs.github.io/charts
+                url: https://openebs-archive.github.io/charts
 "
 
 embed_cluster_config() {
