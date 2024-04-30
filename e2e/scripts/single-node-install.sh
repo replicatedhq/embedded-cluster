0152ff6f69b7ccd5933f26730b7ec1fb4bed60c7
Andrew Lavery <laverya@umich.edu>
2024-05-01 00:48:05 +0800

    update label, again

--- e2e/scripts/single-node-install.sh
+++ e2e/scripts/single-node-install.sh
@@ -241,8 +241,8 @@ ensure_binary_copy() {
 
 ensure_installation_label() {
     # ensure that the installation has the kots backup label
-    if ! kubectl get installations -l "replicated.com/embedded-cluster-backup=infrastructure" --no-headers; then
-        echo "installation does not have the replicated.com/embedded-cluster-backup=infrastructure label"
+    if ! kubectl get installations -l "replicated.com/disaster-recovery=infra" --no-headers; then
+        echo "installation does not have the replicated.com/disaster-recovery=infra label"
         kubectl describe installations --no-headers
         return 1
     fi
--- pkg/addons/embeddedclusteroperator/embeddedclusteroperator.go
+++ pkg/addons/embeddedclusteroperator/embeddedclusteroperator.go
@@ -193,7 +193,7 @@ func (e *EmbeddedClusterOperator) Outro(ctx context.Context, cli client.Client) 
 		ObjectMeta: metav1.ObjectMeta{
 			Name: time.Now().Format("20060102150405"),
 			Labels: map[string]string{
-				"replicated.com/embedded-cluster-backup": "infrastructure",
+				"replicated.com/disaster-recovery": "infra",
 			},
 		},
 		Spec: embeddedclusterv1beta1.InstallationSpec{
