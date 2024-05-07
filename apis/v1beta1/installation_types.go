655aa726aa89d24dd055df77d4f976b1b6dc8e4c
Ricardo Maraschini <ricardo.maraschini@gmail.com>
2024-05-07 21:09:23 +0200

    bug: use k8s yaml library
    use k8s yaml library.

--- apis/v1beta1/installation_types.go
+++ apis/v1beta1/installation_types.go
@@ -19,9 +19,9 @@ package v1beta1
 import (
 	"fmt"
 
-	"gopkg.in/yaml.v2"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/yaml"
 )
 
 // What follows is a list of all valid states for an Installation object.
