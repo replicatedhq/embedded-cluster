8b890c605bfcfa7d69e897c1e12c77c8f786c04f
Salah Aldeen Al Saleh <sg.alsaleh@gmail.com>
2024-05-09 11:58:01 -0700

    fix join commands tests mismatch

--- e2e/playwright/tests/get-join-worker-command/test.spec.ts
+++ e2e/playwright/tests/get-join-worker-command/test.spec.ts
@@ -1,13 +1,13 @@
 import { test, expect } from '@playwright/test';
 import { login } from '../shared';
 
-test('get join controller command', async ({ page }) => {
+test('get join worker command', async ({ page }) => {
   await login(page);
   await page.locator('.NavItem').getByText('Cluster Management', { exact: true }).click();
   await page.getByRole('button', { name: 'Add node', exact: true }).click();
   await expect(page.locator('.Modal-body')).toBeVisible();
   await expect(page.getByRole('heading')).toContainText('Add a Node');
-  await page.locator('.BoxedCheckbox').getByText('controller-test', { exact: true }).click();
+  await page.locator('.BoxedCheckbox').getByText('abc', { exact: true }).click();
   const joinCommand = await page.locator('.react-prism.language-bash').first().textContent();
   console.log(`{"command":"${joinCommand}"}`);
 });
