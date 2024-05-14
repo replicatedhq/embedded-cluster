07167114f79285eab4270070d7dab3c2a8fcfcf1
Ricardo Maraschini <ricardo.maraschini@gmail.com>
2024-05-14 14:59:56 +0200

    feat: apply built in overrides before end user overrides (#610)
    we should apply the release built in overrides before applying the
    overrides we read from the command line. we were only applying the
    latter before this patch so this also solves a bug.
    
    this commit also adds unit tests for the built in overrides and an
    a validation after the installation e2e test.

--- pkg/addons/applier.go
+++ pkg/addons/applier.go
@@ -115,15 +115,6 @@ func (a *Applier) GenerateHelmConfigs(additionalCharts []v1beta1.Chart, addition
 
 	// charts required by the application
 	charts = append(charts, additionalCharts...)
-	if a.endUserConfig != nil {
-		for i, chart := range charts {
-			values, err := a.endUserConfig.Spec.ApplyEndUserAddOnOverrides(chart.Name, chart.Values)
-			if err != nil {
-				return nil, nil, fmt.Errorf("unable to apply end user overrides for %s: %w", chart.Name, err)
-			}
-			charts[i].Values = values
-		}
-	}
 	repositories = append(repositories, additionalRepositories...)
 
 	return charts, repositories, nil
