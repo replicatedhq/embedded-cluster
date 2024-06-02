8d45253b42a52d97ee3f4246a5dade85b98d58ed
Salah Al Saleh <sg.alsaleh@gmail.com>
2024-06-02 04:36:35 +0000

    update operator version

--- e2e/scripts/unsupported-overrides.sh
+++ e2e/scripts/unsupported-overrides.sh
@@ -46,7 +46,7 @@ spec:
                 name: embedded-cluster-operator
                 namespace: embedded-cluster
                 order: 2
-                version: 0.33.2
+                version: 0.34.3
               - chartname: oci://registry.replicated.com/library/admin-console
                 name: admin-console
                 namespace: kotsadm
