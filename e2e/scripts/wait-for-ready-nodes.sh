b3c28edc5b44f86bd3b600bdd5717a60f66e3e1f
Salah Al Saleh <sg.alsaleh@gmail.com>
2024-06-06 20:12:16 +0000

    wait for dns ready

--- e2e/scripts/wait-for-ready-nodes.sh
+++ e2e/scripts/wait-for-ready-nodes.sh
@@ -2,9 +2,23 @@
 # This script waits for X nodes to be ready. X is the first argument.
 set -euox pipefail
 
+wait_for_dns_ready() {
+    local url="$1"
+    local max_retries=10
+    local retries=0
+    while [ "$retries" -lt "$max_retries" ]; do
+        if curl -s -o /dev/null -I -w "%{http_code}" "$url" | grep -q 200; then
+            return 0
+        fi
+        retries=$((retries+1))
+        sleep 10
+    done
+    return 1
+}
+
 main() {
     expected_nodes="$1"
-    skip_goldpinger="${2:-}"
+    is_restore="${2:-}"
 
     ready=$(kubectl get nodes | grep -v NotReady | grep -c Ready || true)
     counter=0
@@ -21,7 +35,10 @@ main() {
     done
     echo "All nodes are ready"
 
-    if [ "$skip_goldpinger" == "true" ]; then
+    if [ "$is_restore" == "true" ]; then
+        # this is a restore operation where the app hasn't been restored yet, so goldpinger won't exist
+        # wait for DNS lookup to work. (this shouldn't be necessary, but CI breaks without it)
+        wait_for_dns_ready "https://staging.replicated.app"
         exit 0
     fi
 
