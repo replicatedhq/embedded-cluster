fc4d9f9ea823c46a562f9ef6fc9dac916354dced
Andrew Lavery <laverya@umich.edu>
2024-06-11 03:08:48 +1000

    add comment

--- types/release_metadata.go
+++ types/release_metadata.go
@@ -8,7 +8,7 @@ type ReleaseMetadata struct {
 	Versions       map[string]string
 	K0sSHA         string
 	K0sBinaryURL   string
-	BinaryURLs     map[string]string
+	BinaryURLs     map[string]string // key is the binary name, value is the URL
 	K0sImages      []string
 	Configs        v1beta1.HelmExtensions            // always applied
 	BuiltinConfigs map[string]v1beta1.HelmExtensions // applied if the relevant builtin addon is enabled
