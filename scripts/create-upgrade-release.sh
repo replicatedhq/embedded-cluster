7dbd561327009b80ff03adb26c6b90ce2236e18a
Ethan Mosbaugh <ethan@replicated.com>
2024-06-14 06:18:31 -0700

    f

--- scripts/create-upgrade-release.sh
+++ scripts/create-upgrade-release.sh
@@ -44,9 +44,7 @@ function metadata() {
     if [ -f metadata.json ]; then
         sudo apt-get install jq -y
 
-        jq '(.Configs.charts[] | select(.name == "embedded-cluster-operator")).values += "resources:\n  requests:\n    cpu: 123m"' metadata.json > upgrade-metadata.json
-        sed -i 's/"Installer": "'"${EC_VERSION_ORIG_NOV}"'",/"Installer": "'"${EC_VERSION_NOV}"'",/g' upgrade-metadata.json
-        sed -i 's/embeddedClusterVersion: '"${EC_VERSION_ORIG_NOV}"'/embeddedClusterVersion: '"${EC_VERSION_NOV}"'/g' upgrade-metadata.json
+        jq '(.Configs.charts[] | select(.name == "embedded-cluster-operator")).values += "resources:\n  requests:\n    cpu: 123m"' metadata-upgrade.json > upgrade-metadata.json
         cat upgrade-metadata.json
 
         retry 3 aws s3 cp upgrade-metadata.json "s3://${S3_BUCKET}/metadata/${EC_VERSION}.json"
@@ -71,10 +69,6 @@ function embeddedcluster() {
 }
 
 function main() {
-    export EC_VERSION_ORIG_NOV
-    # shellcheck disable=SC2001
-    EC_VERSION_ORIG_NOV="$(echo "$EC_VERSION" | sed 's/^v//')"
-    export EC_VERSION_NOV="${EC_VERSION_ORIG_NOV}-upgrade"
     export EC_VERSION="${EC_VERSION}-upgrade"
     metadata
     embeddedcluster
