b5bf4b07b39e7100c32a60164689f1cd14658800
Andrew Lavery <laverya@umich.edu>
2024-06-21 22:38:03 +1000

    cache compressed versions of the kots and embedded-cluster-operator binaries

--- scripts/cache-files.sh
+++ scripts/cache-files.sh
@@ -84,8 +84,13 @@ function operatorbin() {
     echo "downloading embedded cluster operator binary from https://github.com/replicatedhq/embedded-cluster-operator/releases/download/v${operator_version}/manager"
     curl --fail-with-body -L -o "${operator_version}" "https://github.com/replicatedhq/embedded-cluster-operator/releases/download/v${operator_version}/manager"
 
+    chmod +x "${operator_version}"
+
+    # compress the operator binary
+    tar -czf "${operator_version}.tar.gz" "${operator_version}"
+
     # upload the binary to the bucket
-    retry 3 aws s3 cp "${operator_version}" "s3://${S3_BUCKET}/operator-binaries/${operator_version}"
+    retry 3 aws s3 cp "${operator_version}.tar.gz" "s3://${S3_BUCKET}/operator-binaries/${operator_version}.tar.gz"
 }
 
 function kotsbin() {
@@ -117,11 +122,8 @@ function kotsbin() {
         curl --fail-with-body -L -o "kots_linux_amd64.tar.gz" "https://github.com/replicatedhq/kots/releases/download/${kots_version}/kots_linux_amd64.tar.gz"
     fi
 
-    # decompress the bundle, as we only care about the binary and not the sbom/license/readme
-    tar -xvf kots_linux_amd64.tar.gz
-
     # upload the binary to the bucket
-    retry 3 aws s3 cp "kots" "s3://${S3_BUCKET}/kots-binaries/${kots_version}"
+    retry 3 aws s3 cp "kots" "s3://${S3_BUCKET}/kots-binaries/${kots_version}.tar.gz"
 }
 
 function metadata() {
