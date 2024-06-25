f7ea0e9e4dea76bfa2b141d2f3be6ea0c8f8c072
Andrew Lavery <laverya@umich.edu>
2024-06-25 13:11:28 -0400

    implement pod/service cidr check

--- e2e/scripts/check-cidr-ranges.sh
+++ e2e/scripts/check-cidr-ranges.sh
@@ -2,16 +2,24 @@
 set -euox pipefail
 
 main() {
-    local pod_cidr="$1"
-    local service_cidr="$2"
+    local pod_cidr_matcher="$1"
+    local service_cidr_matcher="$2"
     sleep 10 # wait for kubectl to become available
 
     echo "nodes:"
     kubectl get nodes -o wide
-    echo "pods cidr: $pod_cidr"
+    echo "pods cidr: $pod_cidr_matcher"
     kubectl get pods -A -o wide
-    echo "services cidr: $service_cidr"
+    if ! kubectl get pods -A -o jsonpath='{.items[*].status.podIP}' | grep -e "$pod_cidr_matcher" ; then
+        echo "pods not found in CIDR"
+        return 1
+    fi
+    echo "services cidr: $service_cidr_matcher"
     kubectl get services -A
+    if ! kubectl get services -A -o jsonpath='{.items[*].spec.clusterIP}' | grep -e "$service_cidr_matcher" ; then
+        echo "services not found in CIDR"
+        return 1
+    fi
 }
 
 export EMBEDDED_CLUSTER_METRICS_BASEURL="https://staging.replicated.app"
