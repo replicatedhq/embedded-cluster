8a41ef74329b2e893efeec92f769eaef9f1edc77
Ethan Mosbaugh <ethan@replicated.com>
2024-06-26 15:27:32 -0700

    quiet aws s3 cp

--- scripts/create-previous-k0s-release.sh
+++ scripts/create-previous-k0s-release.sh
@@ -47,7 +47,7 @@ function metadata() {
         jq '(.Configs.charts[] | select(.name == "embedded-cluster-operator")).values += "resources:\n  requests:\n    cpu: 123m"' metadata-previous-k0s.json > install-metadata.json
         cat install-metadata.json
 
-        retry 3 aws s3 cp install-metadata.json "s3://${S3_BUCKET}/metadata/${EC_VERSION}.json"
+        retry 3 aws s3 cp --no-progress install-metadata.json "s3://${S3_BUCKET}/metadata/${EC_VERSION}.json"
     else
         echo "metadata-previous-k0s.json not found, skipping upload"
     fi
@@ -62,7 +62,7 @@ function embeddedcluster() {
     # check if a file 'embedded-cluster-linux-amd64-previous-k0s.tgz' exists in the directory
     # if it does, upload it as releases/${ec_version}.tgz
     if [ -f embedded-cluster-linux-amd64-previous-k0s.tgz ]; then
-        retry 3 aws s3 cp embedded-cluster-linux-amd64-previous-k0s.tgz "s3://${S3_BUCKET}/releases/${EC_VERSION}.tgz"
+        retry 3 aws s3 cp --no-progress embedded-cluster-linux-amd64-previous-k0s.tgz "s3://${S3_BUCKET}/releases/${EC_VERSION}.tgz"
     else
         echo "embedded-cluster-linux-amd64-previous-k0s.tgz not found, skipping upload"
     fi
