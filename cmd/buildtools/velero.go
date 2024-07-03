c331af9d1c7c0c19da8adfc7059858945a1a581e
Ricardo Maraschini <ricardo.maraschini@gmail.com>
2024-07-03 14:34:46 +0200

    chore: undo temporary velero mirroring

--- cmd/buildtools/velero.go
+++ cmd/buildtools/velero.go
@@ -13,37 +13,34 @@ var updateVeleroAddonCommand = &cli.Command{
 	UsageText: environmentUsageText,
 	Action: func(c *cli.Context) error {
 		logrus.Infof("updating velero addon")
-		/*
-			latest, err := GetLatestGitHubTag(c.Context, "vmware-tanzu", "velero")
-			if err != nil {
-				return fmt.Errorf("failed to get latest velero release: %w", err)
-			}
-			if err := SetMakefileVariable("VELERO_IMAGE_VERSION", latest); err != nil {
-				return fmt.Errorf("failed to set velero image version: %w", err)
-			}
+		latest, err := GetLatestGitHubTag(c.Context, "vmware-tanzu", "velero")
+		if err != nil {
+			return fmt.Errorf("failed to get latest velero release: %w", err)
+		}
+		if err := SetMakefileVariable("VELERO_IMAGE_VERSION", latest); err != nil {
+			return fmt.Errorf("failed to set velero image version: %w", err)
+		}
 
-			latest, err = GetLatestGitHubTag(c.Context, "vmware-tanzu", "velero-plugin-for-aws")
-			if err != nil {
-				return fmt.Errorf("failed to get latest velero plugin release: %w", err)
-			}
-			if err := SetMakefileVariable("VELERO_AWS_PLUGIN_IMAGE_VERSION", latest); err != nil {
-				return fmt.Errorf("failed to set velero plugin version: %w", err)
-			}
+		latest, err = GetLatestGitHubTag(c.Context, "vmware-tanzu", "velero-plugin-for-aws")
+		if err != nil {
+			return fmt.Errorf("failed to get latest velero plugin release: %w", err)
+		}
+		if err := SetMakefileVariable("VELERO_AWS_PLUGIN_IMAGE_VERSION", latest); err != nil {
+			return fmt.Errorf("failed to set velero plugin version: %w", err)
+		}
 
-			latest, err = LatestChartVersion("vmware-tanzu", "velero")
-			if err != nil {
-				return fmt.Errorf("unable to get the latest registry version: %v", err)
-			}
+		latest, err = LatestChartVersion("vmware-tanzu", "velero")
+		if err != nil {
+			return fmt.Errorf("unable to get the latest registry version: %v", err)
+		}
 
-			if original, err := GetMakefileVariable("VELERO_CHART_VERSION"); err != nil {
-				return fmt.Errorf("unable to get velero chart version: %w", err)
-			} else if original == latest && !c.Bool("force") {
-				logrus.Infof("velero chart is up to date: %s", original)
-				return nil
-			}
-		*/
+		if original, err := GetMakefileVariable("VELERO_CHART_VERSION"); err != nil {
+			return fmt.Errorf("unable to get velero chart version: %w", err)
+		} else if original == latest && !c.Bool("force") {
+			logrus.Infof("velero chart is up to date: %s", original)
+			return nil
+		}
 
-		latest := "6.3.0"
 		if err := MirrorChart("vmware-tanzu", "velero", latest); err != nil {
 			return fmt.Errorf("unable to mirror velero chart: %w", err)
 		}
