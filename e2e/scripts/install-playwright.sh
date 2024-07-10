995461313465d77874a1475a12ba2dd41d65ddae
Ethan Mosbaugh <ethan@replicated.com>
2024-07-10 06:29:37 -0700

    upgrade test

--- e2e/scripts/install-playwright.sh
+++ e2e/scripts/install-playwright.sh
@@ -1,21 +1,32 @@
 #!/usr/bin/env bash
 set -euox pipefail
 
-main() {
-    apt-get update -y
-    apt-get install -y \
-        ca-certificates \
-        curl \
-        gnupg
+install_apt() {
+    curl -fsSL "https://deb.nodesource.com/setup_${NODE_MAJOR}.x" -o nodesource_setup.sh
+    bash nodesource_setup.sh
+    apt-get install -y nodejs
+}
 
-    curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-    NODE_MAJOR=20
-    echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
-    apt-get update && apt-get install nodejs -y
+install_yum() {
+    curl -fsSL "https://rpm.nodesource.com/setup_${NODE_MAJOR}.x" -o nodesource_setup.sh
+    bash nodesource_setup.sh
+    yum install -y nodejs
+}
+
+main() {
+    if command -v apt-get &> /dev/null; then
+        install_apt
+    elif command -v yum &> /dev/null; then
+        install_yum
+    else
+        echo "Unsupported package manager"
+        exit 1
+    fi
 
     cd /automation/playwright
     npm ci
     npx playwright install --with-deps
 }
 
+export NODE_MAJOR=20
 main "$@"
