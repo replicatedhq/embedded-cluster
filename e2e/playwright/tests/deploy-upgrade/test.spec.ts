6803208884a46c165197105917adc7cd7a9adfc3
Salah Aldeen Al Saleh <sg.alsaleh@gmail.com>
2024-07-13 12:08:50 -0700

    use correct locator

--- e2e/playwright/tests/deploy-upgrade/test.spec.ts
+++ e2e/playwright/tests/deploy-upgrade/test.spec.ts
@@ -5,10 +5,7 @@ test('deploy upgrade', async ({ page }) => {
   test.setTimeout(15 * 60 * 1000); // 15 minutes
   await login(page);
   await page.getByRole('link', { name: 'Version history', exact: true }).click();
-  // TODO NOW: uncomment this
-  // await page.locator('.available-update-row', { hasText: process.env.APP_UPGRADE_VERSION }).getByRole('button', { name: 'Deploy', exact: true }).click();
-  // TODO NOW: remove this line
-  await page.getByText(process.env.APP_UPGRADE_VERSION, { exact: true }).locator('../../..').getByRole('button', { name: 'Deploy', exact: true }).click();
+  await page.locator('.available-update-row', { hasText: process.env.APP_UPGRADE_VERSION }).getByRole('button', { name: 'Deploy', exact: true }).click();
   const iframe = page.frameLocator('#upgrade-service-iframe');
   await expect(iframe.locator('h3')).toContainText('The First Config Group', { timeout: 20 * 1000 });
   await expect(iframe.locator('input[type="text"]')).toHaveValue('initial-hostname.com');
@@ -33,7 +30,6 @@ test('deploy upgrade', async ({ page }) => {
   }
 
   await expect(page.locator('.available-update-row', { hasText: process.env.APP_UPGRADE_VERSION })).not.toBeVisible({ timeout: 10 * 1000 });
-  await expect(page.locator('.VersionHistoryRow', { hasText: process.env.APP_UPGRADE_VERSION })).toContainText('Deploying');
   await expect(page.locator('.VersionHistoryRow', { hasText: process.env.APP_UPGRADE_VERSION })).toContainText('Currently deployed version', { timeout: 90 * 1000 });
   await page.getByRole('link', { name: 'Dashboard', exact: true }).click();
   await expect(page.locator('.VersionCard-content--wrapper')).toContainText(process.env.APP_UPGRADE_VERSION);
