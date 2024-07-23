8ad461d5dc337b07786e22b94cb86fa2b44cb629
Ethan Mosbaugh <ethan@replicated.com>
2024-07-23 13:37:11 -0700

    f

--- pkg/addons/velero/velero.go
+++ pkg/addons/velero/velero.go
@@ -107,14 +107,11 @@ func (o *Velero) GenerateHelmConfig(onlyDefaults bool) ([]eckinds.Chart, []eckin
 
 func (o *Velero) GetAdditionalImages() []string {
 	var images []string
-	for _, img := range []string{
-		"docker.io/bitnami/kubectl", "velero/velero-restore-helper",
-	} {
-		tag, ok := Metadata.Images[img]
-		if !ok {
-			continue
-		}
-		images = append(images, fmt.Sprintf("proxy.replicated.com/anonymous/%s:%s", img, tag))
+	if tag, ok := Metadata.Images["velero-restore-helper"]; ok {
+		images = append(images, fmt.Sprintf("proxy.replicated.com/anonymous/replicated/ec-velero-restore-helper:%s", tag))
+	}
+	if tag, ok := Metadata.Images["kubectl"]; ok {
+		images = append(images, fmt.Sprintf("proxy.replicated.com/anonymous/replicated/ec-kubectl:%s", tag))
 	}
 	return images
 }
