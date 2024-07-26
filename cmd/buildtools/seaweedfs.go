efafa4b00c0fa3ac86410fbf687d6ff8a1e9df79
Salah Al Saleh <sg.alsaleh@gmail.com>
2024-07-25 21:31:32 -0700

    Build seaweedfs image with chainguard (#842)
    * Build seaweedfs image with Chainguard

--- cmd/buildtools/seaweedfs.go
+++ cmd/buildtools/seaweedfs.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"strings"
 
+	"github.com/Masterminds/semver/v3"
 	"github.com/replicatedhq/embedded-cluster/pkg/addons/seaweedfs"
 	"github.com/replicatedhq/embedded-cluster/pkg/release"
 	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli/v2"
 )
+
+var seaweedfsImageComponents = map[string]string{
+	"docker.io/chrislusf/seaweedfs": "seaweedfs",
+}
+
+var seaweedfsComponents = map[string]addonComponent{
+	"seaweedfs": {
+		getWolfiPackageName: func(k0sVersion *semver.Version, upstreamVersion *semver.Version) string {
+			return "seaweedfs"
+		},
+		upstreamVersionInputOverride: "INPUT_SEAWEEDFS_VERSION",
+	},
+}
 
 var updateSeaweedFSAddonCommand = &cli.Command{
 	Name:      "seaweedfs",
@@ -18,62 +33,147 @@ var updateSeaweedFSAddonCommand = &cli.Command{
 	Action: func(c *cli.Context) error {
 		logrus.Infof("updating seaweedfs addon")
 
-		latest, err := LatestChartVersion("seaweedfs", "seaweedfs")
-		if err != nil {
-			return fmt.Errorf("unable to get the latest seaweedfs version: %v", err)
+		nextChartVersion := os.Getenv("INPUT_SEAWEEDFS_CHART_VERSION")
+		if nextChartVersion != "" {
+			logrus.Infof("using input override from INPUT_SEAWEEDFS_CHART_VERSION: %s", nextChartVersion)
+		} else {
+			logrus.Infof("fetching the latest seaweedfs chart version")
+			latest, err := LatestChartVersion("seaweedfs", "seaweedfs")
+			if err != nil {
+				return fmt.Errorf("failed to get the latest seaweedfs chart version: %v", err)
+			}
+			nextChartVersion = latest
+			logrus.Printf("latest seaweedfs chart version: %s", latest)
 		}
-		latest = strings.TrimPrefix(latest, "v")
-		logrus.Infof("found seaweedfs chart version %s", latest)
+		nextChartVersion = strings.TrimPrefix(nextChartVersion, "v")
 
 		current := seaweedfs.Metadata
-		if current.Version == latest && !c.Bool("force") {
-			logrus.Infof("seaweedfs chart is up to date")
-			return nil
-		}
-
-		logrus.Infof("mirroring seaweedfs chart")
-		if err := MirrorChart("seaweedfs", "seaweedfs", latest); err != nil {
-			return fmt.Errorf("unable to mirror seaweedfs chart: %w", err)
+		if current.Version == nextChartVersion && !c.Bool("force") {
+			logrus.Infof("seaweedfs chart version is already up-to-date")
+		} else {
+			logrus.Infof("mirroring seaweedfs chart version %s", nextChartVersion)
+			if err := MirrorChart("seaweedfs", "seaweedfs", nextChartVersion); err != nil {
+				return fmt.Errorf("failed to mirror seaweedfs chart: %v", err)
+			}
 		}
 
 		upstream := fmt.Sprintf("%s/seaweedfs", os.Getenv("CHARTS_DESTINATION"))
-		newmeta := release.AddonMetadata{
-			Version:  latest,
-			Location: fmt.Sprintf("oci://proxy.replicated.com/anonymous/%s", upstream),
-			Images:   make(map[string]string),
-		}
+		withproto := fmt.Sprintf("oci://proxy.replicated.com/anonymous/%s", upstream)
 
-		values, err := release.GetValuesWithOriginalImages("seaweedfs")
+		logrus.Infof("updating seaweedfs images")
+
+		err := updateSeaweedFSAddonImages(c.Context, withproto, nextChartVersion)
 		if err != nil {
-			return fmt.Errorf("unable to get openebs values: %v", err)
+			return fmt.Errorf("failed to update seaweedfs images: %w", err)
 		}
 
-		logrus.Infof("extracting images from chart")
-		withproto := fmt.Sprintf("oci://%s", upstream)
-		images, err := GetImagesFromOCIChart(withproto, "seaweedfs", latest, values)
-		if err != nil {
-			return fmt.Errorf("failed to get images from admin console chart: %w", err)
-		}
+		logrus.Infof("successfully updated seaweedfs addon")
 
-		logrus.Infof("fetching digest for images")
-		for _, image := range images {
-			sha, err := GetImageDigest(c.Context, image)
-			if err != nil {
-				return fmt.Errorf("failed to get image %s digest: %w", image, err)
-			}
-			logrus.Infof("image %s digest: %s", image, sha)
-			tag := TagFromImage(image)
-			image = RemoveTagFromImage(image)
-			newmeta.Images[image] = fmt.Sprintf("%s@%s", tag, sha)
-		}
-
-		logrus.Infof("saving addon manifest")
-		newmeta.ReplaceImages = true
-		if err := newmeta.Save("seaweedfs"); err != nil {
-			return fmt.Errorf("failed to save metadata: %w", err)
-		}
-
-		logrus.Infof("successfully updated seaweed addon")
 		return nil
 	},
+}
+
+var updateSeaweedFSImagesCommand = &cli.Command{
+	Name:      "seaweedfs",
+	Usage:     "Updates the seaweedfs images",
+	UsageText: environmentUsageText,
+	Action: func(c *cli.Context) error {
+		logrus.Infof("updating seaweedfs images")
+
+		current := seaweedfs.Metadata
+
+		err := updateSeaweedFSAddonImages(c.Context, current.Location, current.Version)
+		if err != nil {
+			return fmt.Errorf("failed to update seaweedfs images: %w", err)
+		}
+
+		logrus.Infof("successfully updated seaweedfs images")
+
+		return nil
+	},
+}
+
+func updateSeaweedFSAddonImages(ctx context.Context, chartURL string, chartVersion string) error {
+	newmeta := release.AddonMetadata{
+		Version:  chartVersion,
+		Location: chartURL,
+		Images:   make(map[string]string),
+	}
+
+	k0sVersion, err := getK0sVersion()
+	if err != nil {
+		return fmt.Errorf("failed to get k0s version: %w", err)
+	}
+
+	logrus.Infof("fetching wolfi apk index")
+	wolfiAPKIndex, err := GetWolfiAPKIndex()
+	if err != nil {
+		return fmt.Errorf("failed to get APK index: %w", err)
+	}
+
+	values, err := release.GetValuesWithOriginalImages("seaweedfs")
+	if err != nil {
+		return fmt.Errorf("failed to get seaweedfs values: %v", err)
+	}
+
+	logrus.Infof("extracting images from chart version %s", chartVersion)
+	images, err := GetImagesFromOCIChart(chartURL, "seaweedfs", chartVersion, values)
+	if err != nil {
+		return fmt.Errorf("failed to get images from seaweedfs chart: %w", err)
+	}
+
+	if err := ApkoLogin(); err != nil {
+		return fmt.Errorf("failed to apko login: %w", err)
+	}
+
+	for _, image := range images {
+		logrus.Infof("updating image %s", image)
+
+		upstreamVersion := TagFromImage(image)
+		image = RemoveTagFromImage(image)
+
+		componentName, ok := seaweedfsImageComponents[image]
+		if !ok {
+			return fmt.Errorf("no component found for image %s", image)
+		}
+
+		component, ok := seaweedfsComponents[componentName]
+		if !ok {
+			return fmt.Errorf("no component found for component name %s", componentName)
+		}
+
+		if component.upstreamVersionInputOverride != "" {
+			v := os.Getenv(component.upstreamVersionInputOverride)
+			if v != "" {
+				logrus.Infof("using input override from %s: %s", component.upstreamVersionInputOverride, v)
+				upstreamVersion = v
+			}
+		}
+
+		packageName, packageVersion, err := component.getPackageNameAndVersion(wolfiAPKIndex, k0sVersion, upstreamVersion)
+		if err != nil {
+			return fmt.Errorf("failed to get package name and version for %s: %w", componentName, err)
+		}
+
+		logrus.Infof("building and publishing %s, %s=%s", componentName, packageName, packageVersion)
+
+		if err := ApkoBuildAndPublish(componentName, packageName, packageVersion, upstreamVersion); err != nil {
+			return fmt.Errorf("failed to apko build and publish for %s: %w", componentName, err)
+		}
+
+		digest, err := GetDigestFromBuildFile()
+		if err != nil {
+			return fmt.Errorf("failed to get digest from build file: %w", err)
+		}
+
+		newmeta.Images[componentName] = fmt.Sprintf("%s@%s", packageVersion, digest)
+	}
+
+	logrus.Infof("saving addon manifest")
+	newmeta.ReplaceImages = true
+	if err := newmeta.Save("seaweedfs"); err != nil {
+		return fmt.Errorf("failed to save metadata: %w", err)
+	}
+
+	return nil
 }
