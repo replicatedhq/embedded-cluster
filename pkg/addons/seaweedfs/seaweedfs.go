16270f0a70bb6c9a02013c4c2387336db4e6385f
Ethan Mosbaugh <ethan@replicated.com>
2024-07-25 22:30:36 -0700

    f

--- pkg/addons/seaweedfs/seaweedfs.go
+++ pkg/addons/seaweedfs/seaweedfs.go
@@ -13,6 +13,7 @@ import (
 	"k8s.io/apimachinery/pkg/util/wait"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
+	"github.com/replicatedhq/embedded-cluster/pkg/helpers"
 	"github.com/replicatedhq/embedded-cluster/pkg/kubeutils"
 	"github.com/replicatedhq/embedded-cluster/pkg/release"
 	"github.com/replicatedhq/embedded-cluster/pkg/spinner"
@@ -95,8 +96,8 @@ func (o *SeaweedFS) GenerateHelmConfig(onlyDefaults bool) ([]eckinds.Chart, []ec
 
 func (a *SeaweedFS) GetImages() []string {
 	var images []string
-	for image, tag := range Metadata.Images {
-		images = append(images, fmt.Sprintf("proxy.replicated.com/anonymous/%s:%s", image, tag))
+	for component, tag := range Metadata.Images {
+		images = append(images, fmt.Sprintf("%s:%s", helpers.AddonImageFromComponentName(component), tag))
 	}
 	return images
 }
