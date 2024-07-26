c09143751a2b3a77fa5067c0826d88dbf3e4ec30
Andrew Lavery <laverya@umich.edu>
2024-07-26 12:07:58 -0400

    move a lot of test code to common.sh

--- e2e/scripts/default-install.sh
+++ e2e/scripts/default-install.sh
@@ -1,50 +1,8 @@
 #!/usr/bin/env bash
 set -euox pipefail
 
-wait_for_healthy_node() {
-    ready=$(kubectl get nodes | grep -v NotReady | grep -c Ready || true)
-    counter=0
-    while [ "$ready" -lt "1" ]; do
-        if [ "$counter" -gt 36 ]; then
-            return 1
-        fi
-        sleep 5
-        counter=$((counter+1))
-        echo "Waiting for node to be ready"
-        ready=$(kubectl get nodes | grep -v NotReady | grep -c Ready || true)
-        kubectl get nodes || true
-    done
-
-    return 0
-}
-
-wait_for_pods_running() {
-    local timeout="$1"
-    local start_time
-    local current_time
-    local elapsed_time
-    start_time=$(date +%s)
-    while true; do
-        current_time=$(date +%s)
-        elapsed_time=$((current_time - start_time))
-        if [ "$elapsed_time" -ge "$timeout" ]; then
-            kubectl get pods -A -o yaml || true
-            kubectl describe nodes || true
-            echo "Timed out waiting for all pods to be running."
-            return 1
-        fi
-        local non_running_pods
-        non_running_pods=$(kubectl get pods --all-namespaces --no-headers 2>/dev/null | awk '$4 != "Running" && $4 != "Completed" { print $0 }' | wc -l || echo 1)
-        if [ "$non_running_pods" -ne 0 ]; then
-            echo "Not all pods are running. Waiting."
-            kubectl get pods,nodes -A || true
-            sleep 5
-            continue
-        fi
-        echo "All pods are running."
-        return 0
-    done
-}
+DIR=/usr/local/bin
+. $DIR/common.sh
 
 check_openebs_storage_class() {
     scs=$(kubectl get sc --no-headers | wc -l)
