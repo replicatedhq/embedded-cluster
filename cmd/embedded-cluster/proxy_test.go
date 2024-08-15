5a9d1112819d2d360be30b2fd1f1e27cd7e6b9da
Ethan Mosbaugh <ethan@replicated.com>
2024-08-15 09:35:17 -0700

    f

--- cmd/embedded-cluster/proxy_test.go
+++ cmd/embedded-cluster/proxy_test.go
@@ -40,7 +40,7 @@ func Test_getProxySpecFromFlags(t *testing.T) {
 			want: &ecv1beta1.ProxySpec{
 				HTTPProxy:  "http://proxy",
 				HTTPSProxy: "https://proxy",
-				NoProxy:    "localhost,127.0.0.1,.default,.local,.svc,kubernetes,kotsadm-rqlite,no-proxy-1,no-proxy-2,1.1.1.1/22,2.2.2.2/22",
+				NoProxy:    "localhost,127.0.0.1,.default,.local,.svc,kubernetes,kotsadm-rqlite,no-proxy-1,no-proxy-2,10.244.0.0/16,10.96.0.0/12",
 			},
 		},
 		{
@@ -57,22 +57,7 @@ func Test_getProxySpecFromFlags(t *testing.T) {
 			want: &ecv1beta1.ProxySpec{
 				HTTPProxy:  "http://other-proxy",
 				HTTPSProxy: "https://other-proxy",
-				NoProxy:    "localhost,127.0.0.1,.default,.local,.svc,kubernetes,kotsadm-rqlite,other-no-proxy-1,other-no-proxy-2,1.1.1.1/22,2.2.2.2/22",
-			},
-		},
-		{
-			name: "no cluster config should not set proxy",
-			init: func(t *testing.T, flagSet *flag.FlagSet) {
-				t.Setenv("HTTP_PROXY", "http://proxy")
-				t.Setenv("HTTPS_PROXY", "https://proxy")
-				t.Setenv("NO_PROXY", "no-proxy-1,no-proxy-2")
-
-				flagSet.Set("proxy", "true")
-			},
-			want: &ecv1beta1.ProxySpec{
-				HTTPProxy:  "http://proxy",
-				HTTPSProxy: "https://proxy",
-				NoProxy:    "localhost,127.0.0.1,.default,.local,.svc,kubernetes,kotsadm-rqlite,no-proxy-1,no-proxy-2",
+				NoProxy:    "localhost,127.0.0.1,.default,.local,.svc,kubernetes,kotsadm-rqlite,other-no-proxy-1,other-no-proxy-2,10.244.0.0/16,10.96.0.0/12",
 			},
 		},
 		{
@@ -90,7 +75,23 @@ func Test_getProxySpecFromFlags(t *testing.T) {
 			want: &ecv1beta1.ProxySpec{
 				HTTPProxy:  "http://other-proxy",
 				HTTPSProxy: "https://other-proxy",
-				NoProxy:    "localhost,127.0.0.1,.default,.local,.svc,kubernetes,kotsadm-rqlite,no-proxy-1,no-proxy-2,other-no-proxy-1,other-no-proxy-2,1.1.1.1/22,2.2.2.2/22",
+				NoProxy:    "localhost,127.0.0.1,.default,.local,.svc,kubernetes,kotsadm-rqlite,no-proxy-1,no-proxy-2,other-no-proxy-1,other-no-proxy-2,10.244.0.0/16,10.96.0.0/12",
+			},
+		},
+		{
+			name: "pod and service CIDR should override default no proxy",
+			init: func(t *testing.T, flagSet *flag.FlagSet) {
+				flagSet.Set("http-proxy", "http://other-proxy")
+				flagSet.Set("https-proxy", "https://other-proxy")
+				flagSet.Set("no-proxy", "other-no-proxy-1,other-no-proxy-2")
+
+				flagSet.Set("pod-cidr", "1.1.1.1/24")
+				flagSet.Set("service-cidr", "2.2.2.2/24")
+			},
+			want: &ecv1beta1.ProxySpec{
+				HTTPProxy:  "http://other-proxy",
+				HTTPSProxy: "https://other-proxy",
+				NoProxy:    "localhost,127.0.0.1,.default,.local,.svc,kubernetes,kotsadm-rqlite,other-no-proxy-1,other-no-proxy-2,1.1.1.1/24,2.2.2.2/24",
 			},
 		},
 	}
