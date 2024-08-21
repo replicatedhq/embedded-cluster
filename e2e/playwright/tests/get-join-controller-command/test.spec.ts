861dfa8fa5273e304a711b8a724bacc31d51ab1c
miaawong <miawongdev@gmail.com>
2024-08-21 11:52:38 -0400

    update tests

--- e2e/playwright/tests/get-join-controller-command/test.spec.ts
+++ e2e/playwright/tests/get-join-controller-command/test.spec.ts
@@ -4,10 +4,9 @@ import { login } from '../shared';
 test('get join controller command', async ({ page }) => {
   await login(page);
   await page.locator('.NavItem').getByText('Cluster Management', { exact: true }).click();
-  await page.getByRole('button', { name: 'Add node', exact: true }).click();
-  await expect(page.locator('.Modal-body')).toBeVisible();
-  await expect(page.getByRole('heading')).toContainText('Add a Node');
-  await page.locator('.BoxedCheckbox').getByText('controller-test', { exact: true }).click();
+  await expect(page.getByText("Optionally add nodes to the cluster"),).toBeVisible();
+  await expect(page.getByText("Roles:")).toBeVisible();
+  await page.getByText("controller-test", { exact: true }).toBeChecked()
   const joinCommand = await page.locator('.react-prism.language-bash').first().textContent();
   console.log(`{"command":"${joinCommand}"}`);
 });
--- e2e/playwright/tests/get-join-worker-command/test.spec.ts
+++ e2e/playwright/tests/get-join-worker-command/test.spec.ts
@@ -4,10 +4,11 @@ import { login } from '../shared';
 test('get join worker command', async ({ page }) => {
   await login(page);
   await page.locator('.NavItem').getByText('Cluster Management', { exact: true }).click();
-  await page.getByRole('button', { name: 'Add node', exact: true }).click();
-  await expect(page.locator('.Modal-body')).toBeVisible();
-  await expect(page.getByRole('heading')).toContainText('Add a Node');
-  await page.locator('.BoxedCheckbox').getByText('abc', { exact: true }).click();
+  await expect(page.getByText("Optionally add nodes to the cluster"),).toBeVisible();
+  await expect(page.getByText("Roles:")).toBeVisible();
+  await page.getByText("controller-test", { exact: true }).click();
+  await page.getByText("abc", { exact: true }).click();
+  await expect(page.getByLabel("abc")).toBeChecked();
   const joinCommand = await page.locator('.react-prism.language-bash').first().textContent();
   console.log(`{"command":"${joinCommand}"}`);
 });
