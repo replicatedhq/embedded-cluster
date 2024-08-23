62e117795e0474d9d94906b5474a59b9dab711a1
Ethan Mosbaugh <ethan@replicated.com>
2024-08-23 10:46:53 -0700

    fix(ci): cluster bundle is broken in airgap mode

--- e2e/scripts/collect-support-bundle-cluster.sh
+++ e2e/scripts/collect-support-bundle-cluster.sh
@@ -2,10 +2,14 @@
 set -euox pipefail
 
 main() {
-    local url="https://raw.githubusercontent.com/replicatedhq/embedded-cluster-operator/main/charts/embedded-cluster-operator/troubleshoot/cluster-support-bundle.yaml"
-    if ! kubectl support-bundle --output cluster.tar.gz --interactive=false --load-cluster-specs "$url" ; then
-        echo "Failed to collect cluster support bundle"
-        return 1
+    if ! kubectl support-bundle --output cluster.tar.gz --interactive=false --load-cluster-specs ; then
+        # NOTE: this will fail in airgap but we've already failed above
+        # TODO: improve this by downloading the spec through the proxy and running with a file path
+        local url="https://raw.githubusercontent.com/replicatedhq/embedded-cluster-operator/main/charts/embedded-cluster-operator/troubleshoot/cluster-support-bundle.yaml"
+        if ! kubectl support-bundle --output cluster.tar.gz --interactive=false --load-cluster-specs "$url" ; then
+            echo "Failed to collect cluster support bundle"
+            return 1
+        fi
     fi
 }
 
