458725e52143018416dd3767f15c7336453acbf9
Salah Al Saleh <sg.alsaleh@gmail.com>
2024-08-23 16:42:56 +0000

    debug 1.28 tests

--- e2e/scripts/collect-support-bundle-cluster.sh
+++ e2e/scripts/collect-support-bundle-cluster.sh
@@ -2,8 +2,8 @@
 set -euox pipefail
 
 main() {
-    local url="https://raw.githubusercontent.com/replicatedhq/embedded-cluster-operator/main/charts/embedded-cluster-operator/troubleshoot/cluster-support-bundle.yaml"
-    if ! kubectl support-bundle --output cluster.tar.gz --interactive=false --load-cluster-specs "$url" ; then
+    # local url="https://raw.githubusercontent.com/replicatedhq/embedded-cluster-operator/main/charts/embedded-cluster-operator/troubleshoot/cluster-support-bundle.yaml"
+    if ! kubectl support-bundle --output cluster.tar.gz --interactive=false --load-cluster-specs ; then
         echo "Failed to collect cluster support bundle"
         return 1
     fi
