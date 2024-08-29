0238c6f818a2b9f3b48055e4352d91c6a7500698
Ethan Mosbaugh <ethan@replicated.com>
2024-08-29 09:45:51 -0700

    f

--- scripts/create-previous-k0s-release.sh
+++ scripts/create-previous-k0s-release.sh
@@ -59,10 +59,17 @@ function embeddedcluster() {
         echo "EC_VERSION unset, not uploading embedded cluster release"
         return 0
     fi
+
+    # append a 'v' prefix to the version if it doesn't already have one
+    local version="$EC_VERSION"
+    if ! echo "$version" | grep -q "^v"; then
+        version="v$version"
+    fi
+
     # check if a file 'embedded-cluster-linux-amd64-previous-k0s.tgz' exists in the directory
-    # if it does, upload it as releases/${ec_version}.tgz
+    # if it does, upload it as releases/${version}.tgz
     if [ -f embedded-cluster-linux-amd64-previous-k0s.tgz ]; then
-        retry 3 aws s3 cp --no-progress embedded-cluster-linux-amd64-previous-k0s.tgz "s3://${S3_BUCKET}/releases/${EC_VERSION}.tgz"
+        retry 3 aws s3 cp --no-progress embedded-cluster-linux-amd64-previous-k0s.tgz "s3://${S3_BUCKET}/releases/${version}.tgz"
     else
         echo "embedded-cluster-linux-amd64-previous-k0s.tgz not found, skipping upload"
     fi
