d47b0e0e8d25160591bc00936eb5540d3bda3a51
Ethan Mosbaugh <ethan@replicated.com>
2024-09-05 13:59:29 -0700

    f

--- scripts/ci-embed-release.sh
+++ scripts/ci-embed-release.sh
@@ -3,7 +3,7 @@
 # shellcheck source=./common.sh
 source ./scripts/common.sh
 
-set -euo pipefail
+set -euxo pipefail
 
 EC_VERSION=${EC_VERSION:-}
 APP_VERSION=${APP_VERSION:-}
--- scripts/dev-build.sh
+++ scripts/dev-build.sh
@@ -2,6 +2,10 @@
 
 set -euo pipefail
 
+export EC_VERSION APP_VERSION
+EC_VERSION="$(git describe --tags --match='[0-9]*.[0-9]*.[0-9]*')"
+APP_VERSION="appver-dev-$(git rev-parse --short HEAD)"
+
 ./scripts/ci-build-deps.sh
 ./scripts/ci-build.sh
 ./scripts/ci-embed-release.sh
