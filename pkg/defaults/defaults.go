835bc7cca410b5cdf51ff1a4b3ebfb12daa59ffd
Ethan Mosbaugh <ethan@replicated.com>
2024-09-09 12:45:08 -0700

    f

--- pkg/defaults/defaults.go
+++ pkg/defaults/defaults.go
@@ -14,7 +14,7 @@ var (
 )
 
 // Holds the default no proxy values.
-var DefaultNoProxy = []string{"localhost", "127.0.0.1", ".cluster.local"}
+var DefaultNoProxy = []string{"localhost", "127.0.0.1", ".cluster.local.", ".cluster.local", ".svc"}
 
 const ProxyRegistryAddress = "proxy.replicated.com"
 
