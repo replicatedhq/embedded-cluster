76b8b257ec0699af79521e8760b39a555e9c24f3
Andrew Lavery <laverya@umich.edu>
2024-09-10 14:15:45 -0400

    rearrange join-after-restore awaits

--- e2e/playwright/tests/get-restore-join-controller-command/test.spec.ts
+++ e2e/playwright/tests/get-restore-join-controller-command/test.spec.ts
@@ -3,10 +3,11 @@ import { login } from '../shared';
 
 test('get restore join controller command', async ({ page }) => {
   await login(page);
+  await expect(page.locator('#controller-testNodeType')).toBeChecked();
+  await expect(page.locator('.CodeSnippet-copy')).toBeVisible();
   await expect(page.getByText('Nodes')).toBeVisible();
   await expect(page.getByText('Select one or more roles to assign to the new node')).toBeVisible();
   await expect(page.getByText('Roles:')).toBeVisible();
-  await expect(page.locator('#controller-testNodeType')).toBeChecked();
   const joinCommand = await page.locator('.react-prism.language-bash').first().textContent();
   console.log(`{"command":"${joinCommand}"}`);
 });
