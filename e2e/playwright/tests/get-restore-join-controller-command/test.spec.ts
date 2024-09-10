bf75b43032f9f1cb5aa720ae16f89a1c1f121de7
Andrew Lavery <laverya@umich.edu>
2024-09-10 15:09:44 -0400

    remove unneeded selectors

--- e2e/playwright/tests/get-restore-join-controller-command/test.spec.ts
+++ e2e/playwright/tests/get-restore-join-controller-command/test.spec.ts
@@ -5,9 +5,6 @@ test('get restore join controller command', async ({ page }) => {
   await login(page);
   await expect(page.locator('#controller-testNodeType')).toBeChecked();
   await expect(page.locator('.CodeSnippet-copy')).toBeVisible();
-  await expect(page.getByText('Nodes')).toBeVisible();
-  await expect(page.getByText('Select one or more roles to assign to the new node')).toBeVisible();
-  await expect(page.getByText('Roles:')).toBeVisible();
   const joinCommand = await page.locator('.react-prism.language-bash').first().textContent();
   console.log(`{"command":"${joinCommand}"}`);
 });
