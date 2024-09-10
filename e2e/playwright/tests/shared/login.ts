ce4db49071ccebad77ea8120cad588341c5f6725
Andrew Lavery <laverya@umich.edu>
2024-09-10 13:22:13 -0400

    Update login.ts

--- e2e/playwright/tests/shared/login.ts
+++ e2e/playwright/tests/shared/login.ts
@@ -3,5 +3,4 @@ export const login = async (page) => {
   await page.getByPlaceholder('password').click();
   await page.getByPlaceholder('password').fill('password');
   await page.getByRole('button', { name: 'Log in' }).click();
-  await page.waitForTimeout(3000); // sleep before reloading page
 };
