070059cfc0e39e068080a37c4bdf544a917d7bde
Andrew Lavery <laverya@umich.edu>
2024-09-18 08:35:47 -0500

    add more unit test coverage of the preflight

--- pkg/preflights/app_test.go
+++ pkg/preflights/app_test.go
@@ -43,6 +43,46 @@ spec:
 `)},
 			wantErr: "failed to parse helm chart values for addon test as yaml: error converting YAML to JSON: yaml: invalid map key: map[interface {}]interface {}{\"repl RandomString 10\":interface {}(nil)}",
 		},
+		{
+			name: "good extension values",
+			releaseData: map[string][]byte{
+				"embedded-cluster-config.yaml": []byte(`
+apiVersion: embeddedcluster.replicated.com/v1beta1
+kind: Config
+metadata:
+  name: "testconfig"
+spec:
+  extensions:
+    helm:
+      charts:
+      - chartname: test
+        name: test
+        namespace: test
+        version: 1.0.0
+        values: |
+          goodyaml: true
+`)},
+			wantErr: "",
+		},
+		{
+			name: "no extension values",
+			releaseData: map[string][]byte{
+				"embedded-cluster-config.yaml": []byte(`
+apiVersion: embeddedcluster.replicated.com/v1beta1
+kind: Config
+metadata:
+  name: "testconfig"
+spec:
+  extensions:
+    helm:
+      charts:
+      - chartname: test
+        name: test
+        namespace: test
+        version: 1.0.0
+`)},
+			wantErr: "",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
