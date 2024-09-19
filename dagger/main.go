70c58475ae194af2ff2be34e7323c960852fa5d5
Ethan Mosbaugh <ethan@replicated.com>
2024-09-19 13:27:13 -0700

    chore(ci): build and publish operator and lam image with dagger

--- dagger/main.go
+++ dagger/main.go
@@ -1,53 +1,15 @@
 package main
 
-import (
-	"context"
-
-	"dagger/embedded-cluster/internal/dagger"
-)
+import "dagger/embedded-cluster/internal/dagger"
 
 const (
+	APKOImageVersion    = "latest"
 	MelangeImageVersion = "latest"
 )
 
 type EmbeddedCluster struct {
 	common
-	melange
-}
-
-// Builds the operator package with Melange.
-func (m *EmbeddedCluster) BuildOperatorPackage(
-	ctx context.Context,
-	// Source directory to use for the build.
-	// +defaultPath="/"
-	src *dagger.Directory,
-	// Version to use for the package.
-	ecVersion string,
-	// Architectures to build for.
-	// +default="amd64,arm64"
-	arch string,
-) *dagger.Directory {
-
-	melangeFile := m.renderTemplate(
-		src.Directory("operator/deploy"),
-		map[string]string{
-			"PACKAGE_VERSION": ecVersion,
-		},
-		"melange.tmpl.yaml",
-		"melange.yaml",
-	)
-
-	dir := dag.Directory().
-		WithDirectory("operator", src.Directory("operator"))
-
-	build := m.melangeBuildGo(ctx,
-		directoryWithCommonGoFiles(dir, src),
-		melangeFile,
-		arch,
-		MelangeImageVersion,
-	)
-
-	return build.Directory("build")
+	chainguard
 }
 
 // directoryWithCommonGoFiles sets up the filesystem with only what we need to build for improved
