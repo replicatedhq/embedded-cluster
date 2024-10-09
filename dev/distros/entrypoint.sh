4aab3a3109f4d527bb06530b68466ef1a64258d3
Salah Aldeen Al Saleh <sg.alsaleh@gmail.com>
2024-10-09 11:21:46 -0700

    Disable unnecessary flaky systemd services

--- dev/distros/entrypoint.sh
+++ dev/distros/entrypoint.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-# Disable getty service as it's flaky and doesn't apply in containers
-systemctl mask getty@tty1.service
+# Disable unneeded services
+systemctl mask \
+    apt-daily-upgrade.service \
+    apt-daily.service \
+    dpkg-db-backup.service \
+    getty-static.service \
+    getty@tty1.service \
+    console-getty.service \
+    systemd-firstboot.service \
+    systemd-ask-password-console.service \
+    systemd-ask-password-wall.service \
+    emergency.service \
+    rescue.service
 
 # A unique machine ID is required for multi-node clusters in k0s <= v1.29
 # https://github.com/k0sproject/k0s/blob/443e28b75d216e120764136b4513e6237cea7cc5/docs/external-runtime-deps.md#a-unique-machine-id-for-multi-node-setups
