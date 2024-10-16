5d6495f14b5a09e3feb54073907550e7960d3c3b
Ricardo Maraschini <ricardo.maraschini@gmail.com>
2024-10-16 21:16:32 +0200

    chore: added a test to verify custom cidr in no-proxy
    no-proxy should contain the custom cidr provided by the user.

--- cmd/embedded-cluster/proxy_test.go
+++ cmd/embedded-cluster/proxy_test.go
@@ -109,6 +109,22 @@ func Test_getProxySpecFromFlags(t *testing.T) {
 				NoProxy:         "localhost,127.0.0.1,.cluster.local,.svc,other-no-proxy-1,other-no-proxy-2,1.1.1.1/24,2.2.2.2/24",
 			},
 		},
+		{
+			name: "custom --cidr should be present in the no-proxy",
+			init: func(t *testing.T, flagSet *flag.FlagSet) {
+				flagSet.Set("http-proxy", "http://other-proxy")
+				flagSet.Set("https-proxy", "https://other-proxy")
+				flagSet.Set("no-proxy", "other-no-proxy-1,other-no-proxy-2")
+
+				flagSet.Set("cidr", "10.0.0.0/16")
+			},
+			want: &ecv1beta1.ProxySpec{
+				HTTPProxy:       "http://other-proxy",
+				HTTPSProxy:      "https://other-proxy",
+				ProvidedNoProxy: "other-no-proxy-1,other-no-proxy-2",
+				NoProxy:         "localhost,127.0.0.1,.cluster.local,.svc,other-no-proxy-1,other-no-proxy-2,10.0.0.0/17,10.0.128.0/17",
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
