e34008ef936b8a12e41e2474101e96721c3640b9
Andrew Lavery <laverya@umich.edu>
2024-10-15 21:30:25 -0400

    debugging

--- operator/pkg/charts/detection.go
+++ operator/pkg/charts/detection.go
@@ -102,15 +102,18 @@ func DetectChartCompletion(existingHelm *v1beta1.HelmExtensions, installedCharts
 					return nil, nil, fmt.Errorf("failed to compare values of chart %s: %w", chart.Name, err)
 				}
 				if valuesDiff {
+					fmt.Printf("Chart %s values differ from the current values\n", chart.Name)
 					diffDetected = true
 				}
 
 				// if the spec's HashValues does not match the status's ValuesHash, the chart is currently being applied with the new values
 				if installedChart.Spec.HashValues() != installedChart.Status.ValuesHash {
+					fmt.Printf("Chart %s is currently being applied with new values\n", chart.Name)
 					diffDetected = true
 				}
 
 				if installedChart.Status.Version != chart.Version {
+					fmt.Printf("Chart %s version is different from the current version\n", chart.Name)
 					diffDetected = true
 				}
 
