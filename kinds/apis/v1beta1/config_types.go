8039e218bfd44cd5acbf2a43276f9afb680eebe2
Ethan Mosbaugh <ethan@replicated.com>
2024-10-17 14:55:28 -0700

    f

--- kinds/apis/v1beta1/config_types.go
+++ kinds/apis/v1beta1/config_types.go
@@ -90,10 +90,8 @@ type Chart struct {
 	// +kubebuilder:validation:Optional
 	Values   string `json:"values,omitempty"`
 	TargetNS string `json:"namespace,omitempty"`
-	// Timeout specifies the timeout for how long to wait for the chart installation to finish.
-	// A duration string is a sequence of decimal numbers, each with optional fraction and a unit suffix, such as "300ms" or "2h45m". Valid time units are "ns", "us" (or "µs"), "ms", "s", "m", "h".
-	// +kubebuilder:validation:XIntOrString
-	Timeout BackwardCompatibleDuration `json:"timeout,omitempty"`
+	// +kubebuilder:validation:Optional
+	Timeout time.Duration `json:"timeout,omitempty"`
 	// ForceUpgrade when set to false, disables the use of the "--force" flag when upgrading the the chart (default: true).
 	// +optional
 	ForceUpgrade *bool `json:"forceUpgrade,omitempty"`
