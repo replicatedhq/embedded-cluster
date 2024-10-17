09c96734a2c5aee445b17a3431b8491c4f703fa4
ada mancini <ada@replicated.com>
2024-10-17 11:22:35 -0400

    remove privateCAs() from Applier

--- pkg/addons/applier.go
+++ pkg/addons/applier.go
@@ -252,11 +252,6 @@ func (a *Applier) HostPreflightsForRestore() (*v1beta2.HostPreflightSpec, error)
 	return a.hostPreflights(addons)
 }
 
-// PrivateCAs returns a list of private CAs for the cluster.
-func (a *Applier) PrivateCAs() map[string]string {
-	return a.privateCAs
-}
-
 func (a *Applier) hostPreflights(addons []AddOn) (*v1beta2.HostPreflightSpec, error) {
 	allpf := &v1beta2.HostPreflightSpec{}
 	for _, addon := range addons {
