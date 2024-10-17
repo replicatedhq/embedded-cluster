b5bed1c57972cbeb0f2fb68f5357fb6d74c074f7
Ethan Mosbaugh <ethan@replicated.com>
2024-10-17 13:43:17 -0700

    f

--- e2e/install_test.go
+++ e2e/install_test.go
@@ -611,13 +611,13 @@ func TestUpgradeEC18FromReplicatedApp(t *testing.T) {
 
 	t.Logf("%s: resetting worker node", time.Now().Format(time.RFC3339))
 	line = []string{"reset-installation.sh"}
-	if stdout, stderr, err := tc.RunCommandOnNode(1, line); err != nil {
+	if stdout, stderr, err := tc.RunCommandOnNode(1, line, withEnv); err != nil {
 		t.Fatalf("fail to reset worker node: %v: %s: %s", err, stdout, stderr)
 	}
 
 	t.Logf("%s: resetting node 0", time.Now().Format(time.RFC3339))
 	line = []string{"reset-installation.sh"}
-	if stdout, stderr, err := tc.RunCommandOnNode(0, line); err != nil {
+	if stdout, stderr, err := tc.RunCommandOnNode(0, line, withEnv); err != nil {
 		t.Fatalf("fail to reset node 0: %v: %s: %s", err, stdout, stderr)
 	}
 
@@ -1037,11 +1037,17 @@ func TestAirgapUpgradeFromEC18(t *testing.T) {
 		t.Logf("failed to remove airgap upgrade bundle: %v", err)
 	}
 
+	// upgrade airgap bundle is only needed on the first node
+	line := []string{"rm", "/assets/ec-release-upgrade.tgz"}
+	if _, _, err := tc.RunCommandOnNode(1, line); err != nil {
+		t.Fatalf("fail to remove upgrade airgap bundle on node %s: %v", tc.Nodes[1], err)
+	}
+
 	// install "curl" dependency on node 0 for app version checks.
 	tc.InstallTestDependenciesDebian(t, 0, true)
 
 	t.Logf("%s: preparing embedded cluster airgap files", time.Now().Format(time.RFC3339))
-	line := []string{"airgap-prepare.sh"}
+	line = []string{"airgap-prepare.sh"}
 	if _, _, err := tc.RunCommandOnNode(0, line); err != nil {
 		t.Fatalf("fail to prepare airgap files on node %s: %v", tc.Nodes[0], err)
 	}
@@ -1144,18 +1150,18 @@ func TestAirgapUpgradeFromEC18(t *testing.T) {
 		t.Fatalf("fail to check postupgrade state: %v", err)
 	}
 
-	// use upgrade binary for reset
-	withUpgradeBin := map[string]string{"EMBEDDED_CLUSTER_BIN": "embedded-cluster-upgrade"}
-
 	t.Logf("%s: resetting worker node", time.Now().Format(time.RFC3339))
 	line = []string{"reset-installation.sh"}
-	if stdout, stderr, err := tc.RunCommandOnNode(1, line, withUpgradeBin); err != nil {
+	if stdout, stderr, err := tc.RunCommandOnNode(1, line, withEnv); err != nil {
 		t.Fatalf("fail to reset worker node: %v: %s: %s", err, stdout, stderr)
 	}
 
+	// use upgrade binary for reset
+	withUpgradeBin := map[string]string{"EMBEDDED_CLUSTER_BIN": "embedded-cluster-upgrade"}
+
 	t.Logf("%s: resetting node 0", time.Now().Format(time.RFC3339))
 	line = []string{"reset-installation.sh"}
-	if stdout, stderr, err := tc.RunCommandOnNode(0, line, withUpgradeBin); err != nil {
+	if stdout, stderr, err := tc.RunCommandOnNode(0, line, withEnv, withUpgradeBin); err != nil {
 		t.Fatalf("fail to reset node 0: %v: %s: %s", err, stdout, stderr)
 	}
 
