97328a8c1ad058c089b01a565cd531f9aa5aca1f
Ricardo Maraschini <ricardo.maraschini@gmail.com>
2024-10-24 16:55:22 +0200

    chore: print support bundle tgz location

--- cmd/embedded-cluster/support_bundle.go
+++ cmd/embedded-cluster/support_bundle.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli/v2"
@@ -72,7 +75,45 @@ func supportBundleCommand() *cli.Command {
 
 			spin.Infof("Support bundle collected!")
 			spin.Close()
+
+			printSupportBundlePath(stdout)
 			return nil
 		},
+	}
+}
+
+// printSupportBundlePath attempts to parse the support bundle command output
+// and find the location where the tgz was written. support bundle output isn't
+// always a json so we can't rely on unmarshaling it. depending on the kinds of
+// errors returned during the collection the output may differ quite a bunch.
+// in order to find where the support bundle was stored we look for a line
+// containing "archivePath", if we find it we attempt to parse it. if we can't
+// find a match or failed to parse it we simply don't print anything. XXX this
+// should be addressed upstream, we should be able to cleanly parse the output
+// as a json (i.e. errors should be printed into stderr).
+func printSupportBundlePath(stdout io.Reader) {
+	pwd, err := os.Getwd()
+	if err != nil {
+		return
+	}
+
+	scanner := bufio.NewScanner(stdout)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if !strings.Contains(line, `"archivePath":`) {
+			continue
+		}
+
+		components := strings.Split(line, `"`)
+		if len(components) < 4 {
+			return
+		}
+
+		fname := components[3]
+		if !strings.HasPrefix(fname, "support-bundle-") {
+			return
+		}
+
+		logrus.Infof("Support bundle written to %s", filepath.Join(pwd, fname))
 	}
 }
