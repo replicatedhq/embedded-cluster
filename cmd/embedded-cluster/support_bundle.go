fbcc297e070cb850bcc76b9e7ad9ef0a5eff1ce3
Ricardo Maraschini <ricardo.maraschini@gmail.com>
2024-10-24 18:20:53 +0200

    chore: improvements to support-bundle command (#1395)
    * chore: only set kubeconfig env if a kubeconfig exists
    
    * chore: print a better error message if unable to find the support bundle binary
    
    * chore: print support bundle tgz location
    
    * chore: stop parsing support-bundle output

--- cmd/embedded-cluster/support_bundle.go
+++ cmd/embedded-cluster/support_bundle.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
+	"time"
 
+	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli/v2"
 
 	"github.com/replicatedhq/embedded-cluster/pkg/defaults"
@@ -29,14 +32,29 @@ func supportBundleCommand() *cli.Command {
 
 			supportBundle := provider.PathToEmbeddedClusterBinary("kubectl-support_bundle")
 			if _, err := os.Stat(supportBundle); err != nil {
-				return fmt.Errorf("unable to find support bundle binary")
+				logrus.Errorf("Support bundle binary not found. The support-bundle command can only be run after an 'install' attempt.")
+				return ErrNothingElseToAdd
 			}
 
-			kubeConfig := provider.PathToKubeConfig()
 			hostSupportBundle := provider.PathToEmbeddedClusterSupportFile("host-support-bundle.yaml")
 			if _, err := os.Stat(hostSupportBundle); err != nil {
 				return fmt.Errorf("unable to find host support bundle: %w", err)
 			}
+
+			kubeConfig := provider.PathToKubeConfig()
+			var env map[string]string
+			if _, err := os.Stat(kubeConfig); err == nil {
+				// if we have a kubeconfig, use it.
+				env = map[string]string{"KUBECONFIG": kubeConfig}
+			}
+
+			pwd, err := os.Getwd()
+			if err != nil {
+				return fmt.Errorf("unable to get current working directory: %w", err)
+			}
+			now := time.Now().Format("2006-01-02T15_04_05")
+			fname := fmt.Sprintf("support-bundle-%s.tar.gz", now)
+			destination := filepath.Join(pwd, fname)
 
 			spin := spinner.Start()
 			spin.Infof("Collecting support bundle (this may take a while)")
@@ -48,11 +66,12 @@ func supportBundleCommand() *cli.Command {
 					Writer:       stdout,
 					ErrWriter:    stderr,
 					LogOnSuccess: true,
-					Env:          map[string]string{"KUBECONFIG": kubeConfig},
+					Env:          env,
 				},
 				supportBundle,
 				"--interactive=false",
 				"--load-cluster-specs",
+				fmt.Sprintf("--output=%s", destination),
 				hostSupportBundle,
 			); err != nil {
 				spin.Infof("Failed to collect support bundle")
@@ -62,7 +81,7 @@ func supportBundleCommand() *cli.Command {
 				return ErrNothingElseToAdd
 			}
 
-			spin.Infof("Support bundle collected!")
+			spin.Infof("Support bundle saved at %s", destination)
 			spin.Close()
 			return nil
 		},
