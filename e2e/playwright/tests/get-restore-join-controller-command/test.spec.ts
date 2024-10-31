b9b283f3d7db1650c7d12787fdd5f3f36876e93b
Ethan Mosbaugh <ethan@replicated.com>
2024-10-31 12:57:47 -0700

    fix playwright tests

--- e2e/playwright/tests/get-restore-join-controller-command/test.spec.ts
+++ e2e/playwright/tests/get-restore-join-controller-command/test.spec.ts
@@ -3,6 +3,7 @@ import { login } from '../shared';
 
 test('get restore join controller command', async ({ page }) => {
   await login(page);
+  await page.getByRole('button', { name: 'Add node', exact: true }).click();
   await expect(page.locator('#controller-testNodeType')).toBeChecked();
   await expect(page.locator('.CodeSnippet-copy')).toBeVisible();
   const joinCommand = await page.locator('.react-prism.language-bash').first().textContent();
