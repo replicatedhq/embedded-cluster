59e97a440d69ca3fbb6377485b5c0a3bceaf49b8
Ethan Mosbaugh <ethan@replicated.com>
2024-12-13 19:13:53 -0600

    chore: add back cli unit tests (#1623)

---
+++ cmd/installer/cli/install_test.go
@@ -0,0 +1,329 @@
+package cli
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"testing"
+
+	"github.com/replicatedhq/embedded-cluster/pkg/prompts"
+	"github.com/replicatedhq/embedded-cluster/pkg/prompts/plain"
+	"github.com/replicatedhq/embedded-cluster/pkg/release"
+	kotsv1beta1 "github.com/replicatedhq/kotskinds/apis/kots/v1beta1"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_validateAdminConsolePassword(t *testing.T) {
+	tests := []struct {
+		name          string
+		password      string
+		passwordCheck string
+		wantSuccess   bool
+	}{
+		{
+			name:          "passwords match, with 3 characters length",
+			password:      "123",
+			passwordCheck: "123",
+			wantSuccess:   false,
+		},
+		{
+			name:          "passwords don't match, with 3 characters length",
+			password:      "123",
+			passwordCheck: "nop",
+			wantSuccess:   false,
+		},
+		{
+			name:          "passwords don't match, with 6 characters length",
+			password:      "123456",
+			passwordCheck: "nmatch",
+			wantSuccess:   false,
+		},
+		{
+			name:          "passwords match, with 6 characters length",
+			password:      "123456",
+			passwordCheck: "123456",
+			wantSuccess:   true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req := require.New(t)
+
+			success := validateAdminConsolePassword(tt.password, tt.passwordCheck)
+			if tt.wantSuccess {
+				req.True(success)
+			} else {
+				req.False(success)
+			}
+		})
+	}
+}
+
+func Test_maybeAskAdminConsolePassword(t *testing.T) {
+
+	tests := []struct {
+		name         string
+		userPassword string
+		noPrompt     bool
+		wantPassword string
+		wantError    bool
+	}{
+		{
+			name:         "no user provided password, no-prompt true",
+			userPassword: "",
+			noPrompt:     true,
+			wantPassword: "password",
+			wantError:    false,
+		},
+		{
+			name:         "invalid user provided password, no-prompt false",
+			userPassword: "123",
+			noPrompt:     false,
+			wantPassword: "",
+			wantError:    true,
+		},
+		{
+			name:         "user provided password, no-prompt true",
+			userPassword: "123456",
+			noPrompt:     true,
+			wantPassword: "123456",
+			wantError:    false,
+		},
+		{
+			name:         "user provided password, no-prompt false",
+			userPassword: "123456",
+			noPrompt:     false,
+			wantPassword: "123456",
+			wantError:    false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req := require.New(t)
+
+			cmd := InstallCmd(context.Background(), "testing")
+
+			flags := cmd.Flags()
+			flags.Set("no-prompt", strconv.FormatBool(tt.noPrompt))
+			flags.Set("admin-console-password", tt.userPassword)
+
+			passwordSet, err := maybeAskAdminConsolePassword(cmd, true)
+			if tt.wantError {
+				req.Error(err)
+			} else {
+				req.Equal(tt.wantPassword, passwordSet)
+			}
+		})
+	}
+}
+
+func Test_maybePromptForAppUpdate(t *testing.T) {
+	tests := []struct {
+		name                  string
+		channelRelease        *release.ChannelRelease
+		apiHandler            func(http.ResponseWriter, *http.Request)
+		confirm               bool
+		wantPrompt            bool
+		wantErr               bool
+		isErrNothingElseToAdd bool
+	}{
+		{
+			name: "current release should return false",
+			channelRelease: &release.ChannelRelease{
+				ChannelID:    "channel-id",
+				ChannelSlug:  "channel-slug",
+				AppSlug:      "app-slug",
+				VersionLabel: "1.0.0",
+			},
+			apiHandler: func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Content-Type", "application/json")
+				_, _ = w.Write([]byte(`{"channelReleases": [
+					{
+						"channelId": "channel-id",
+						"channelSequence": 1,
+						"releaseSequence": 1,
+						"versionLabel": "1.0.0",
+						"isRequired": true,
+						"createdAt": "2023-10-01T00:00:00Z",
+						"releaseNotes": "release notes",
+						"replicatedRegistryDomain": "replicated.app",
+						"replicatedProxyDomain": "replicated.app"
+					}
+				]}`))
+			},
+			confirm:    false,
+			wantPrompt: false,
+			wantErr:    false,
+		},
+		{
+			name: "newer release and confirm should return true",
+			channelRelease: &release.ChannelRelease{
+				ChannelID:    "channel-id",
+				ChannelSlug:  "channel-slug",
+				AppSlug:      "app-slug",
+				VersionLabel: "1.0.0",
+			},
+			apiHandler: func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Content-Type", "application/json")
+				_, _ = w.Write([]byte(`{"channelReleases": [
+					{
+						"channelId": "channel-id",
+						"channelSequence": 2,
+						"releaseSequence": 2,
+						"versionLabel": "2.0.0",
+						"isRequired": true,
+						"createdAt": "2023-10-01T00:00:00Z",
+						"releaseNotes": "release notes",
+						"replicatedRegistryDomain": "replicated.app",
+						"replicatedProxyDomain": "replicated.app"
+					}
+				]}`))
+			},
+			confirm:    true,
+			wantPrompt: true,
+			wantErr:    false,
+		},
+		{
+			name: "newer release and no confirm should return true and error",
+			channelRelease: &release.ChannelRelease{
+				ChannelID:    "channel-id",
+				ChannelSlug:  "channel-slug",
+				AppSlug:      "app-slug",
+				VersionLabel: "1.0.0",
+			},
+			apiHandler: func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Content-Type", "application/json")
+				_, _ = w.Write([]byte(`{"channelReleases": [
+					{
+						"channelId": "channel-id",
+						"channelSequence": 2,
+						"releaseSequence": 2,
+						"versionLabel": "2.0.0",
+						"isRequired": true,
+						"createdAt": "2023-10-01T00:00:00Z",
+						"releaseNotes": "release notes",
+						"replicatedRegistryDomain": "replicated.app",
+						"replicatedProxyDomain": "replicated.app"
+					}
+				]}`))
+			},
+			confirm:               false,
+			wantPrompt:            true,
+			wantErr:               true,
+			isErrNothingElseToAdd: true,
+		},
+		{
+			name: "unexpected status code should return error",
+			channelRelease: &release.ChannelRelease{
+				ChannelID:    "channel-id",
+				ChannelSlug:  "channel-slug",
+				AppSlug:      "app-slug",
+				VersionLabel: "1.0.0",
+			},
+			apiHandler: func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusBadRequest)
+			},
+			wantPrompt:            false,
+			wantErr:               true,
+			isErrNothingElseToAdd: false,
+		},
+		{
+			name:                  "no release should return nil",
+			channelRelease:        nil,
+			apiHandler:            func(w http.ResponseWriter, r *http.Request) {},
+			wantPrompt:            false,
+			wantErr:               false,
+			isErrNothingElseToAdd: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var license *kotsv1beta1.License
+			releaseDataMap := map[string][]byte{}
+			if tt.channelRelease != nil {
+				handler := getReleasesHandler(t, tt.channelRelease.ChannelID, tt.apiHandler)
+				ts := httptest.NewServer(handler)
+				t.Cleanup(ts.Close)
+
+				license = &kotsv1beta1.License{
+					Spec: kotsv1beta1.LicenseSpec{
+						LicenseID: "license-id",
+						AppSlug:   "app-slug",
+						Endpoint:  ts.URL,
+					},
+				}
+
+				embedStr := "# channel release object\nchannelID: \"%s\"\nchannelSlug: \"%s\"\nappSlug: \"%s\"\nversionLabel: \"%s\""
+				releaseDataMap["release.yaml"] = []byte(fmt.Sprintf(
+					embedStr,
+					tt.channelRelease.ChannelID,
+					tt.channelRelease.ChannelSlug,
+					tt.channelRelease.AppSlug,
+					tt.channelRelease.VersionLabel,
+				))
+				err := release.SetReleaseDataForTests(releaseDataMap)
+				require.NoError(t, err)
+			}
+			err := release.SetReleaseDataForTests(releaseDataMap)
+			require.NoError(t, err)
+
+			var in *bytes.Buffer
+			if tt.confirm {
+				in = bytes.NewBuffer([]byte("y\n"))
+			} else {
+				in = bytes.NewBuffer([]byte("n\n"))
+			}
+			out := bytes.NewBuffer([]byte{})
+			prompt := plain.New(plain.WithIn(in), plain.WithOut(out))
+
+			prompts.SetTerminal(true)
+			t.Cleanup(func() { prompts.SetTerminal(false) })
+
+			err = maybePromptForAppUpdate(context.Background(), prompt, license, false)
+			if tt.wantErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+
+			if tt.wantPrompt {
+				assert.Contains(t, out.String(), "Do you want to continue installing", "Prompt should have been printed")
+			} else {
+				assert.Empty(t, out.String(), "Prompt should not have been printed")
+			}
+
+			if tt.isErrNothingElseToAdd {
+				assert.Equal(t, ErrNothingElseToAdd, err)
+			} else {
+				assert.NotEqual(t, ErrNothingElseToAdd, err)
+			}
+		})
+	}
+}
+
+func getReleasesHandler(t *testing.T, channelID string, apiHandler http.HandlerFunc) http.HandlerFunc {
+	t.Helper()
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/release/app-slug/pending" {
+			t.Fatalf("unexpected path %s", r.URL.Path)
+		}
+		if r.URL.Query().Get("selectedChannelId") != channelID {
+			t.Fatalf("unexpected selectedChannelId %s", r.URL.Query().Get("selectedChannelId"))
+		}
+		if r.URL.Query().Get("channelSequence") != "" {
+			t.Fatalf("unexpected channelSequence %s", r.URL.Query().Get("channelSequence"))
+		}
+		if r.URL.Query().Get("isSemverSupported") != "true" {
+			t.Fatalf("unexpected isSemverSupported %s", r.URL.Query().Get("isSemverSupported"))
+		}
+
+		apiHandler(w, r)
+	}
+}
---
+++ cmd/installer/cli/proxy_test.go
@@ -0,0 +1,145 @@
+package cli
+
+import (
+	"testing"
+
+	k0sconfig "github.com/k0sproject/k0s/pkg/apis/k0s/v1beta1"
+	ecv1beta1 "github.com/replicatedhq/embedded-cluster/kinds/apis/v1beta1"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_getProxySpecFromFlags(t *testing.T) {
+	type args struct {
+		cfg *k0sconfig.ClusterConfig
+	}
+	tests := []struct {
+		name string
+		init func(t *testing.T, flagSet *pflag.FlagSet)
+		want *ecv1beta1.ProxySpec
+	}{
+		{
+			name: "no flags set should not set proxy",
+			init: func(t *testing.T, flagSet *pflag.FlagSet) {
+				t.Setenv("HTTP_PROXY", "http://proxy")
+				t.Setenv("HTTPS_PROXY", "https://proxy")
+				t.Setenv("NO_PROXY", "no-proxy-1,no-proxy-2")
+			},
+			want: nil,
+		},
+		{
+			name: "proxy from env flag set should set proxy",
+			init: func(t *testing.T, flagSet *pflag.FlagSet) {
+				t.Setenv("HTTP_PROXY", "http://proxy")
+				t.Setenv("HTTPS_PROXY", "https://proxy")
+				t.Setenv("NO_PROXY", "no-proxy-1,no-proxy-2")
+
+				flagSet.Set("proxy", "true")
+			},
+			want: &ecv1beta1.ProxySpec{
+				HTTPProxy:       "http://proxy",
+				HTTPSProxy:      "https://proxy",
+				ProvidedNoProxy: "no-proxy-1,no-proxy-2",
+				NoProxy:         "localhost,127.0.0.1,.cluster.local,.svc,no-proxy-1,no-proxy-2,10.244.0.0/17,10.244.128.0/17",
+			},
+		},
+		{
+			name: "proxy from env flag set should not set proxy for lowercase env vars",
+			init: func(t *testing.T, flagSet *pflag.FlagSet) {
+				t.Setenv("http_proxy", "http://proxy")
+				t.Setenv("https_proxy", "https://proxy")
+				t.Setenv("no_proxy", "no-proxy-1,no-proxy-2")
+
+				flagSet.Set("proxy", "true")
+			},
+			want: nil,
+		},
+		{
+			name: "proxy flags set should set proxy",
+			init: func(t *testing.T, flagSet *pflag.FlagSet) {
+				t.Setenv("HTTP_PROXY", "http://proxy")
+				t.Setenv("HTTPS_PROXY", "https://proxy")
+				t.Setenv("NO_PROXY", "no-proxy-1,no-proxy-2")
+
+				flagSet.Set("http-proxy", "http://other-proxy")
+				flagSet.Set("https-proxy", "https://other-proxy")
+				flagSet.Set("no-proxy", "other-no-proxy-1,other-no-proxy-2")
+			},
+			want: &ecv1beta1.ProxySpec{
+				HTTPProxy:       "http://other-proxy",
+				HTTPSProxy:      "https://other-proxy",
+				ProvidedNoProxy: "other-no-proxy-1,other-no-proxy-2",
+				NoProxy:         "localhost,127.0.0.1,.cluster.local,.svc,other-no-proxy-1,other-no-proxy-2,10.244.0.0/17,10.244.128.0/17",
+			},
+		},
+		{
+			name: "proxy flags should override proxy from env, but merge no-proxy",
+			init: func(t *testing.T, flagSet *pflag.FlagSet) {
+				t.Setenv("HTTP_PROXY", "http://proxy")
+				t.Setenv("HTTPS_PROXY", "https://proxy")
+				t.Setenv("NO_PROXY", "no-proxy-1,no-proxy-2")
+
+				flagSet.Set("proxy", "true")
+				flagSet.Set("http-proxy", "http://other-proxy")
+				flagSet.Set("https-proxy", "https://other-proxy")
+				flagSet.Set("no-proxy", "other-no-proxy-1,other-no-proxy-2")
+			},
+			want: &ecv1beta1.ProxySpec{
+				HTTPProxy:       "http://other-proxy",
+				HTTPSProxy:      "https://other-proxy",
+				ProvidedNoProxy: "no-proxy-1,no-proxy-2,other-no-proxy-1,other-no-proxy-2",
+				NoProxy:         "localhost,127.0.0.1,.cluster.local,.svc,no-proxy-1,no-proxy-2,other-no-proxy-1,other-no-proxy-2,10.244.0.0/17,10.244.128.0/17",
+			},
+		},
+		{
+			name: "pod and service CIDR should override default no proxy",
+			init: func(t *testing.T, flagSet *pflag.FlagSet) {
+				flagSet.Set("http-proxy", "http://other-proxy")
+				flagSet.Set("https-proxy", "https://other-proxy")
+				flagSet.Set("no-proxy", "other-no-proxy-1,other-no-proxy-2")
+
+				flagSet.Set("pod-cidr", "1.1.1.1/24")
+				flagSet.Set("service-cidr", "2.2.2.2/24")
+			},
+			want: &ecv1beta1.ProxySpec{
+				HTTPProxy:       "http://other-proxy",
+				HTTPSProxy:      "https://other-proxy",
+				ProvidedNoProxy: "other-no-proxy-1,other-no-proxy-2",
+				NoProxy:         "localhost,127.0.0.1,.cluster.local,.svc,other-no-proxy-1,other-no-proxy-2,1.1.1.1/24,2.2.2.2/24",
+			},
+		},
+		{
+			name: "custom --cidr should be present in the no-proxy",
+			init: func(t *testing.T, flagSet *pflag.FlagSet) {
+				flagSet.Set("http-proxy", "http://other-proxy")
+				flagSet.Set("https-proxy", "https://other-proxy")
+				flagSet.Set("no-proxy", "other-no-proxy-1,other-no-proxy-2")
+
+				flagSet.Set("cidr", "10.0.0.0/16")
+			},
+			want: &ecv1beta1.ProxySpec{
+				HTTPProxy:       "http://other-proxy",
+				HTTPSProxy:      "https://other-proxy",
+				ProvidedNoProxy: "other-no-proxy-1,other-no-proxy-2",
+				NoProxy:         "localhost,127.0.0.1,.cluster.local,.svc,other-no-proxy-1,other-no-proxy-2,10.0.0.0/17,10.0.128.0/17",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cmd := &cobra.Command{}
+			addCIDRFlags(cmd)
+			addProxyFlags(cmd)
+
+			flagSet := cmd.Flags()
+			if tt.init != nil {
+				tt.init(t, flagSet)
+			}
+
+			got, err := getProxySpecFromFlags(cmd)
+			assert.NoError(t, err, "unexpected error received")
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
