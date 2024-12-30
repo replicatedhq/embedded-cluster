2054de747be0443e1273c66bd160054854e9fffd
Andrew Lavery <laverya@umich.edu>
2024-12-30 23:25:45 +0100

    simpler license check

--- e2e/scripts/validate-support-bundle.sh
+++ e2e/scripts/validate-support-bundle.sh
@@ -19,26 +19,21 @@ main() {
     tar -zxvf support-bundle-*.tar.gz
     rm -rf support-bundle-*.tar.gz
 
+    echo "checking for the k0s sysinfo file"
     if ! ls support-bundle-*/host-collectors/run-host/k0s-sysinfo.txt; then
         echo "Failed to find 'k0s sysinfo' inside the support bundle generated with the embedded cluster binary"
         return 1
     fi
 
+    echo "checking for the embedded-cluster-operator logs"
     if ! ls support-bundle-*/podlogs/embedded-cluster-operator; then
         echo "Failed to find operator logs inside the support bundle generated with the embedded cluster binary"
         return 1
     fi
 
-    if ! ls support-bundle-*/podlogs/embedded-cluster-operator; then
-        echo "Failed to find operator logs inside the support bundle generated with the embedded cluster binary"
-        return 1
-    fi
-
+    echo "checking for the license file inside the support bundle"
     if ! ls support-bundle-*/host-collectors/embedded-cluster/license.yaml; then
         echo "Failed to find license file inside the support bundle generated with the embedded cluster binary"
-        return 1
-    elif diff /assets/license.yaml support-bundle-*/host-collectors/embedded-cluster/license.yaml; then
-        echo "license file inside the support bundle is not the same as the one in the assets folder"
         return 1
     fi
 }
