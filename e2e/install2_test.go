87429687461e5da5f8aed832104dee3a9961624a
Andrew Lavery <laverya@umich.edu>
2025-01-16 18:11:43 +0100

    refactor different-OS test code

--- e2e/install2_test.go
+++ e2e/install2_test.go
@@ -9,20 +9,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestSingleNodeInstall2UbuntuJammy(t *testing.T) {
-	t.Parallel()
-
-	RequireEnvVars(t, []string{"SHORT_SHA"})
-
-	tc := docker.NewCluster(&docker.ClusterInput{
-		T:            t,
-		Nodes:        1,
-		Distro:       "ubuntu-jammy",
-		LicensePath:  "license.yaml",
-		ECBinaryPath: "../output/bin/embedded-cluster",
-	})
-	defer tc.Cleanup()
-
+func singleNodeInstalUpgradeTest(t *testing.T, tc *docker.Cluster) {
 	t.Logf("%s: installing embedded-cluster on node 0", time.Now().Format(time.RFC3339))
 	line := []string{"single-node-install2.sh", "ui", os.Getenv("SHORT_SHA"), "--admin-console-port", "30002"}
 	if stdout, stderr, err := tc.RunCommandOnNode(0, line); err != nil {
@@ -54,6 +41,45 @@ func TestSingleNodeInstall2UbuntuJammy(t *testing.T) {
 	t.Logf("%s: logging in with the new password", time.Now().Format(time.RFC3339))
 	_, _, err = tc.RunPlaywrightTest("login-with-custom-password", newPassword)
 	require.NoError(t, err, "unable to login with the new password")
+}
 
+func TestSingleNodeInstall2UbuntuJammy(t *testing.T) {
+	t.Parallel()
+
+	RequireEnvVars(t, []string{"SHORT_SHA"})
+
+	tc := docker.NewCluster(&docker.ClusterInput{
+		T:            t,
+		Nodes:        1,
+		Distro:       "ubuntu-jammy",
+		LicensePath:  "license.yaml",
+		ECBinaryPath: "../output/bin/embedded-cluster",
+	})
+	defer tc.Cleanup()
+	singleNodeInstalUpgradeTest(t, tc)
+	t.Logf("%s: test complete", time.Now().Format(time.RFC3339))
+}
+
+func TestSingleNodeInstall2AlmaLinux8(t *testing.T) {
+	t.Parallel()
+
+	RequireEnvVars(t, []string{"SHORT_SHA"})
+
+	tc := docker.NewCluster(&docker.ClusterInput{
+		T:            t,
+		Nodes:        1,
+		Distro:       "almalinux-8",
+		LicensePath:  "license.yaml",
+		ECBinaryPath: "../output/bin/embedded-cluster",
+	})
+	defer tc.Cleanup()
+
+	t.Logf("%s: installing tar", time.Now().Format(time.RFC3339))
+	line := []string{"yum-install-tar.sh"}
+	if stdout, stderr, err := tc.RunCommandOnNode(0, line); err != nil {
+		t.Fatalf("fail to check postupgrade state: %v: %s: %s", err, stdout, stderr)
+	}
+
+	singleNodeInstalUpgradeTest(t, tc)
 	t.Logf("%s: test complete", time.Now().Format(time.RFC3339))
 }
