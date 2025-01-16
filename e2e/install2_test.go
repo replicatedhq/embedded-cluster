5bf3bbcbf6352f460003fb654192d50c18a3ee05
Andrew Lavery <laverya@umich.edu>
2025-01-16 19:32:51 +0100

    add expected-to-fail upgrade tests

--- e2e/install2_test.go
+++ e2e/install2_test.go
@@ -1,6 +1,7 @@
 package e2e
 
 import (
+	"fmt"
 	"os"
 	"testing"
 	"time"
@@ -9,7 +10,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func singleNodeInstalUpgradeTest(t *testing.T, tc *docker.Cluster) {
+func singleNodeInstallTest(t *testing.T, tc *docker.Cluster) {
 	t.Logf("%s: installing embedded-cluster on node 0", time.Now().Format(time.RFC3339))
 	line := []string{"single-node-install2.sh", "ui", os.Getenv("SHORT_SHA"), "--admin-console-port", "30002"}
 	if stdout, stderr, err := tc.RunCommandOnNode(0, line); err != nil {
@@ -43,6 +44,40 @@ func singleNodeInstalUpgradeTest(t *testing.T, tc *docker.Cluster) {
 	require.NoError(t, err, "unable to login with the new password")
 }
 
+func singleNodeInstallUpgradeTest(t *testing.T, tc *docker.Cluster) {
+	t.Logf("%s: installing embedded-cluster on node 0", time.Now().Format(time.RFC3339))
+	line := []string{"single-node-install2.sh", "ui", os.Getenv("SHORT_SHA"), "--admin-console-port", "30002"}
+	if stdout, stderr, err := tc.RunCommandOnNode(0, line); err != nil {
+		t.Fatalf("fail to install embedded-cluster on node 0: %v: %s: %s", err, stdout, stderr)
+	}
+
+	if stdout, stderr, err := tc.SetupPlaywrightAndRunTest("deploy-app"); err != nil {
+		t.Fatalf("fail to run playwright test deploy-app: %v: %s: %s", err, stdout, stderr)
+	}
+
+	// TODO: check installation state
+
+	appUpgradeVersion := fmt.Sprintf("appver-%s-upgrade", os.Getenv("SHORT_SHA"))
+	testArgs := []string{appUpgradeVersion}
+
+	t.Logf("%s: upgrading cluster", time.Now().Format(time.RFC3339))
+	if stdout, stderr, err := tc.RunPlaywrightTest("deploy-upgrade", testArgs...); err != nil {
+		t.Fatalf("fail to run playwright test deploy-app: %v: %s: %s", err, stdout, stderr)
+	}
+
+	// TODO: check installation state
+
+	t.Logf("%s: resetting admin console password", time.Now().Format(time.RFC3339))
+	newPassword := "newpass"
+	line = []string{"embedded-cluster", "admin-console", "reset-password", newPassword}
+	_, _, err := tc.RunCommandOnNode(0, line)
+	require.NoError(t, err, "unable to reset admin console password")
+
+	t.Logf("%s: logging in with the new password", time.Now().Format(time.RFC3339))
+	_, _, err = tc.RunPlaywrightTest("login-with-custom-password", newPassword)
+	require.NoError(t, err, "unable to login with the new password")
+}
+
 func TestSingleNodeInstall2UbuntuJammy(t *testing.T) {
 	t.Parallel()
 
@@ -56,7 +91,7 @@ func TestSingleNodeInstall2UbuntuJammy(t *testing.T) {
 		ECBinaryPath: "../output/bin/embedded-cluster",
 	})
 	defer tc.Cleanup()
-	singleNodeInstalUpgradeTest(t, tc)
+	singleNodeInstallTest(t, tc)
 	t.Logf("%s: test complete", time.Now().Format(time.RFC3339))
 }
 
@@ -80,7 +115,7 @@ func TestSingleNodeInstall2AlmaLinux8(t *testing.T) {
 		t.Fatalf("fail to check postupgrade state: %v: %s: %s", err, stdout, stderr)
 	}
 
-	singleNodeInstalUpgradeTest(t, tc)
+	singleNodeInstallTest(t, tc)
 	t.Logf("%s: test complete", time.Now().Format(time.RFC3339))
 }
 
@@ -97,7 +132,7 @@ func TestSingleNodeInstall2Debian11(t *testing.T) {
 		ECBinaryPath: "../output/bin/embedded-cluster",
 	})
 	defer tc.Cleanup()
-	singleNodeInstalUpgradeTest(t, tc)
+	singleNodeInstallTest(t, tc)
 	t.Logf("%s: test complete", time.Now().Format(time.RFC3339))
 }
 
@@ -114,6 +149,81 @@ func TestSingleNodeInstall2Debian12(t *testing.T) {
 		ECBinaryPath: "../output/bin/embedded-cluster",
 	})
 	defer tc.Cleanup()
-	singleNodeInstalUpgradeTest(t, tc)
+	singleNodeInstallTest(t, tc)
+	t.Logf("%s: test complete", time.Now().Format(time.RFC3339))
+}
+
+func TestSingleNodeInstall2UpgradeUbuntuJammy(t *testing.T) {
+	t.Parallel()
+
+	RequireEnvVars(t, []string{"SHORT_SHA"})
+
+	tc := docker.NewCluster(&docker.ClusterInput{
+		T:            t,
+		Nodes:        1,
+		Distro:       "ubuntu-jammy",
+		LicensePath:  "license.yaml",
+		ECBinaryPath: "../output/bin/embedded-cluster",
+	})
+	defer tc.Cleanup()
+	singleNodeInstallUpgradeTest(t, tc)
+	t.Logf("%s: test complete", time.Now().Format(time.RFC3339))
+}
+
+func TestSingleNodeInstall2UpgradeAlmaLinux8(t *testing.T) {
+	t.Parallel()
+
+	RequireEnvVars(t, []string{"SHORT_SHA"})
+
+	tc := docker.NewCluster(&docker.ClusterInput{
+		T:            t,
+		Nodes:        1,
+		Distro:       "almalinux-8",
+		LicensePath:  "license.yaml",
+		ECBinaryPath: "../output/bin/embedded-cluster",
+	})
+	defer tc.Cleanup()
+
+	t.Logf("%s: installing tar", time.Now().Format(time.RFC3339))
+	line := []string{"yum-install-tar.sh"}
+	if stdout, stderr, err := tc.RunCommandOnNode(0, line); err != nil {
+		t.Fatalf("fail to check postupgrade state: %v: %s: %s", err, stdout, stderr)
+	}
+
+	singleNodeInstallUpgradeTest(t, tc)
+	t.Logf("%s: test complete", time.Now().Format(time.RFC3339))
+}
+
+func TestSingleNodeInstall2UpgradeDebian11(t *testing.T) {
+	t.Parallel()
+
+	RequireEnvVars(t, []string{"SHORT_SHA"})
+
+	tc := docker.NewCluster(&docker.ClusterInput{
+		T:            t,
+		Nodes:        1,
+		Distro:       "debian-bullseye",
+		LicensePath:  "license.yaml",
+		ECBinaryPath: "../output/bin/embedded-cluster",
+	})
+	defer tc.Cleanup()
+	singleNodeInstallUpgradeTest(t, tc)
+	t.Logf("%s: test complete", time.Now().Format(time.RFC3339))
+}
+
+func TestSingleNodeInstall2UpgradeDebian12(t *testing.T) {
+	t.Parallel()
+
+	RequireEnvVars(t, []string{"SHORT_SHA"})
+
+	tc := docker.NewCluster(&docker.ClusterInput{
+		T:            t,
+		Nodes:        1,
+		Distro:       "debian-bookworm",
+		LicensePath:  "license.yaml",
+		ECBinaryPath: "../output/bin/embedded-cluster",
+	})
+	defer tc.Cleanup()
+	singleNodeInstallUpgradeTest(t, tc)
 	t.Logf("%s: test complete", time.Now().Format(time.RFC3339))
 }
