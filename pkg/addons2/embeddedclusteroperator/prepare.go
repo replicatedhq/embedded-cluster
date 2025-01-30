7ef8961d3965af0ead515b611f022af31f5cd960
Ethan Mosbaugh <ethan@replicated.com>
2025-01-30 06:09:38 -0800

    f

--- pkg/addons2/embeddedclusteroperator/prepare.go
+++ pkg/addons2/embeddedclusteroperator/prepare.go
@@ -2,6 +2,8 @@ package embeddedclusteroperator
 
 import (
 	"github.com/pkg/errors"
+	"github.com/replicatedhq/embedded-cluster/pkg/addons2/adminconsole"
+	"github.com/replicatedhq/embedded-cluster/pkg/versions"
 )
 
 func (a *EmbeddedClusterOperator) prepare() error {
@@ -13,5 +15,9 @@ func (a *EmbeddedClusterOperator) prepare() error {
 }
 
 func (a *EmbeddedClusterOperator) generateHelmValues() error {
+	helmValues["kotsVersion"] = adminconsole.Metadata.Version
+	helmValues["embeddedClusterVersion"] = versions.Version
+	helmValues["embeddedClusterK0sVersion"] = versions.K0sVersion
+
 	return nil
 }
