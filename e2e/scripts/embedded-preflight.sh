848295f47f897fd7d62bc6ea09dea70a50e4cd71
Andrew Lavery <laverya@umich.edu>
2025-01-31 13:50:22 -0500

    f

--- e2e/scripts/embedded-preflight.sh
+++ e2e/scripts/embedded-preflight.sh
@@ -132,7 +132,7 @@ has_applied_host_preflight() {
 
 main() {
     embed_preflight "$preflight_with_failure"
-    if /usr/local/bin/embedded-cluster install --yes 2>&1 | tee /tmp/log ; then
+    if /usr/local/bin/embedded-cluster install --yes --license /assets/license.yaml 2>&1 | tee /tmp/log ; then
         cat /tmp/log
         echo "preflight_with_failure: Expected installation to fail"
         exit 1
