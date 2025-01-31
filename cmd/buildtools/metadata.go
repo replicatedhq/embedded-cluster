b9737952053089d2a3db4982e77c84a9647ede71
Andrew Lavery <laverya@umich.edu>
2025-01-31 11:41:31 -0500

    fix linting

--- cmd/buildtools/metadata.go
+++ cmd/buildtools/metadata.go
@@ -43,7 +43,7 @@ var metadataExtractHelmChartImagesCommand = &cli.Command{
 
 		repos := metadata.Configs.Repositories
 		charts := metadata.Configs.Charts
-		//lint:ignore SA1019 Using deprecated BuiltinConfigs for backwards compatibility
+		//nolint:staticcheck // SA1019 explanation Using deprecated BuiltinConfigs for backwards compatibility
 		for _, chart := range metadata.BuiltinConfigs {
 			repos = append(repos, chart.Repositories...)
 			charts = append(charts, chart.Charts...)
