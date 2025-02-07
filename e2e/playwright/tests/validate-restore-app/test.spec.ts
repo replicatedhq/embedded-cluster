89b7385268cdf26ef3c78f5f16b2db7f8eb9423c
Ethan Mosbaugh <ethan@replicated.com>
2025-02-07 06:50:47 -0800

    test

--- e2e/playwright/tests/validate-restore-app/test.spec.ts
+++ e2e/playwright/tests/validate-restore-app/test.spec.ts
@@ -1,6 +1,8 @@
 import { test, expect } from '@playwright/test';
-import { vaidateAppAndClusterReady } from '../shared';
+import { login, vaidateAppAndClusterReady } from '../shared';
 
 test('validate restore app', async ({ page }) => {
+  test.setTimeout(5 * 60 * 1000); // 5 minutes
+  await login(page);
   await vaidateAppAndClusterReady(page, expect, 90 * 1000);
 });
