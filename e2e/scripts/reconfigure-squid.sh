77cb82e4ab7089ee656054ce5eb989ce0932dbe6
Ethan Mosbaugh <ethan@replicated.com>
2025-02-12 08:56:51 -0800

    f

--- e2e/scripts/reconfigure-squid.sh
+++ e2e/scripts/reconfigure-squid.sh
@@ -1,10 +1,49 @@
 #!/bin/bash
 set -euxo pipefail
 
-function main() {
-	sed -i 's/http_access allow localnet/http_access allow whitelist/' /etc/squid/conf.d/ec.conf
+function maybe_install() {
+    local package=$1
+    local command=$2
 
-	squid -k reconfigure
+    if [ -n "$command" ]; then
+        if command -v "$command" ; then
+            echo "$command is already installed"
+            return
+        fi
+    fi
+
+    if command -v apt-get ; then
+        apt_install "$package"
+    elif command -v yum ; then
+        yum_install "$package"
+    else
+        echo "Unsupported package manager"
+        exit 1
+    fi
+}
+
+function apt_install() {
+    local package=$1
+
+    apt-get update
+    apt-get install -y "$package"
+}
+
+function yum_install() {
+    local package=$1
+
+    yum install -y "$package"
+}
+
+function main() {
+    # install curl if it's not already installed
+    maybe_install curl
+
+    # update the squid config to allow the whitelist
+    sed -i 's/http_access allow localnet/http_access allow whitelist/' /etc/squid/conf.d/ec.conf
+
+    # restart the squid service
+    squid -k reconfigure
 
     # validate that the whitelist is working
     # validate that we can access ec-e2e-replicated-app.testcluster.net
