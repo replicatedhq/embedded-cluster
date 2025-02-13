3dae0031330e1dbde303d85c4bc059c5f4d66130
Salah Aldeen Al Saleh <sg.alsaleh@gmail.com>
2025-02-13 14:21:26 -0800

    Preflight check that node IP isn't in pod / service CIDRs

--- cmd/installer/cli/install_runpreflights.go
+++ cmd/installer/cli/install_runpreflights.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 
 	"github.com/replicatedhq/embedded-cluster/pkg/configutils"
+	"github.com/replicatedhq/embedded-cluster/pkg/netutils"
 	"github.com/replicatedhq/embedded-cluster/pkg/preflights"
 	"github.com/replicatedhq/embedded-cluster/pkg/runtimeconfig"
 	"github.com/sirupsen/logrus"
@@ -87,6 +88,11 @@ func runInstallPreflights(ctx context.Context, flags InstallCmdFlags, metricsRep
 		proxyRegistryURL = fmt.Sprintf("https://%s", runtimeconfig.ProxyRegistryAddress)
 	}
 
+	nodeIP, err := netutils.FirstValidAddress(flags.networkInterface)
+	if err != nil {
+		return fmt.Errorf("unable to find first valid address: %w", err)
+	}
+
 	if err := preflights.PrepareAndRun(ctx, preflights.PrepareAndRunOptions{
 		ReplicatedAPIURL:     replicatedAPIURL,
 		ProxyRegistryURL:     proxyRegistryURL,
@@ -94,6 +100,7 @@ func runInstallPreflights(ctx context.Context, flags InstallCmdFlags, metricsRep
 		PodCIDR:              flags.cidrCfg.PodCIDR,
 		ServiceCIDR:          flags.cidrCfg.ServiceCIDR,
 		GlobalCIDR:           flags.cidrCfg.GlobalCIDR,
+		NodeIP:               nodeIP,
 		PrivateCAs:           flags.privateCAs,
 		IsAirgap:             flags.isAirgap,
 		SkipHostPreflights:   flags.skipHostPreflights,
