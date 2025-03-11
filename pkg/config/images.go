2126dddeb9526c326aa5ef1eb99595549165c668
Andrew Lavery <laverya@umich.edu>
2025-03-11 14:59:00 -0500

    override k0s images

--- pkg/config/images.go
+++ pkg/config/images.go
@@ -10,6 +10,7 @@ import (
 	k0sv1beta1 "github.com/k0sproject/k0s/pkg/apis/k0s/v1beta1"
 	"github.com/k0sproject/k0s/pkg/constant"
 	"github.com/replicatedhq/embedded-cluster/pkg/release"
+	"github.com/replicatedhq/embedded-cluster/pkg/runtimeconfig"
 	"gopkg.in/yaml.v2"
 )
 
@@ -53,24 +54,26 @@ func overrideK0sImages(cfg *k0sv1beta1.ClusterConfig) {
 		cfg.Spec.Images = &k0sv1beta1.ClusterImages{}
 	}
 
-	cfg.Spec.Images.CoreDNS.Image = Metadata.Images["coredns"].Repo
+	proxyRegistryDomain := runtimeconfig.ProxyRegistryDomain()
+
+	cfg.Spec.Images.CoreDNS.Image = strings.ReplaceAll(Metadata.Images["coredns"].Repo, "proxy.replicated.com", proxyRegistryDomain)
 	cfg.Spec.Images.CoreDNS.Version = Metadata.Images["coredns"].Tag[runtime.GOARCH]
 
-	cfg.Spec.Images.Calico.Node.Image = Metadata.Images["calico-node"].Repo
+	cfg.Spec.Images.Calico.Node.Image = strings.ReplaceAll(Metadata.Images["calico-node"].Repo, "proxy.replicated.com", proxyRegistryDomain)
 	cfg.Spec.Images.Calico.Node.Version = Metadata.Images["calico-node"].Tag[runtime.GOARCH]
 
-	cfg.Spec.Images.Calico.CNI.Image = Metadata.Images["calico-cni"].Repo
+	cfg.Spec.Images.Calico.CNI.Image = strings.ReplaceAll(Metadata.Images["calico-cni"].Repo, "proxy.replicated.com", proxyRegistryDomain)
 	cfg.Spec.Images.Calico.CNI.Version = Metadata.Images["calico-cni"].Tag[runtime.GOARCH]
 
-	cfg.Spec.Images.Calico.KubeControllers.Image = Metadata.Images["calico-kube-controllers"].Repo
+	cfg.Spec.Images.Calico.KubeControllers.Image = strings.ReplaceAll(Metadata.Images["calico-kube-controllers"].Repo, "proxy.replicated.com", proxyRegistryDomain)
 	cfg.Spec.Images.Calico.KubeControllers.Version = Metadata.Images["calico-kube-controllers"].Tag[runtime.GOARCH]
 
-	cfg.Spec.Images.MetricsServer.Image = Metadata.Images["metrics-server"].Repo
+	cfg.Spec.Images.MetricsServer.Image = strings.ReplaceAll(Metadata.Images["metrics-server"].Repo, "proxy.replicated.com", proxyRegistryDomain)
 	cfg.Spec.Images.MetricsServer.Version = Metadata.Images["metrics-server"].Tag[runtime.GOARCH]
 
-	cfg.Spec.Images.KubeProxy.Image = Metadata.Images["kube-proxy"].Repo
+	cfg.Spec.Images.KubeProxy.Image = strings.ReplaceAll(Metadata.Images["kube-proxy"].Repo, "proxy.replicated.com", proxyRegistryDomain)
 	cfg.Spec.Images.KubeProxy.Version = Metadata.Images["kube-proxy"].Tag[runtime.GOARCH]
 
-	cfg.Spec.Images.Pause.Image = Metadata.Images["pause"].Repo
+	cfg.Spec.Images.Pause.Image = strings.ReplaceAll(Metadata.Images["pause"].Repo, "proxy.replicated.com", proxyRegistryDomain)
 	cfg.Spec.Images.Pause.Version = Metadata.Images["pause"].Tag[runtime.GOARCH]
 }
