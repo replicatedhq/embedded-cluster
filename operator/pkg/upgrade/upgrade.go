7eed2e315d72a21919cef9d6dde214b3ddbf54a0
Ethan Mosbaugh <ethan@replicated.com>
2025-03-24 16:32:46 -0700

    f

--- operator/pkg/upgrade/upgrade.go
+++ operator/pkg/upgrade/upgrade.go
@@ -179,14 +179,24 @@ func updateClusterConfig(ctx context.Context, cli client.Client, in *ecv1beta1.I
 
 	domains := runtimeconfig.GetDomains(in.Spec.Config)
 
+	didUpdate := false
+
 	cfg := config.RenderK0sConfig(domains.ProxyRegistryDomain)
 	if currentCfg.Spec.Images != nil {
-		if reflect.DeepEqual(*currentCfg.Spec.Images, *cfg.Spec.Images) {
-			return nil
+		if !reflect.DeepEqual(*currentCfg.Spec.Images, *cfg.Spec.Images) {
+			currentCfg.Spec.Images = cfg.Spec.Images
+			didUpdate = true
 		}
 	}
 
-	currentCfg.Spec.Images = cfg.Spec.Images
+	if currentCfg.Spec.Network.NodeLocalLoadBalancing == nil || !reflect.DeepEqual(*currentCfg.Spec.Network.NodeLocalLoadBalancing, *cfg.Spec.Network.NodeLocalLoadBalancing) {
+		currentCfg.Spec.Network.NodeLocalLoadBalancing = cfg.Spec.Network.NodeLocalLoadBalancing
+		didUpdate = true
+	}
+
+	if !didUpdate {
+		return nil
+	}
 
 	unstructured, err := helpers.K0sClusterConfigTo129Compat(&currentCfg)
 	if err != nil {
