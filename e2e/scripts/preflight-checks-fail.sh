321e6a8351e48a6946428b13de0528cfb6c1cb2b
Andrew Lavery <laverya@umich.edu>
2025-03-28 14:19:23 -0400

    remove tee

--- e2e/scripts/preflight-checks-fail.sh
+++ e2e/scripts/preflight-checks-fail.sh
@@ -6,8 +6,7 @@ DIR=/usr/local/bin
 
 main() {
     echo "Expecting failing preflight checks"
-    if embedded-cluster install run-preflights --license /assets/license.yaml "$@" 2>&1 | tee /tmp/log ; then
-        cat /tmp/log
+    if embedded-cluster install run-preflights --license /assets/license.yaml "$@" 2>&1 ; then
         echo "preflight_with_failure: Expected installation to fail"
         exit 1
     fi
