db684f6072a0ba4e03b53db1fe02a05f2b8139b2
Andrew Lavery <laverya@umich.edu>
2025-03-28 11:17:40 -0400

    f

--- e2e/scripts/preflight-checks-fail.sh
+++ e2e/scripts/preflight-checks-fail.sh
@@ -6,7 +6,7 @@ DIR=/usr/local/bin
 
 main() {
     echo "Expecting failing preflight checks"
-    if /usr/local/bin/embedded-cluster install run-preflights --license /assets/license.yaml "$@" 2>&1 | tee /tmp/log ; then
+    if embedded-cluster install run-preflights --license /assets/license.yaml "$@" 2>&1 | tee /tmp/log ; then
         cat /tmp/log
         echo "preflight_with_failure: Expected installation to fail"
         exit 1
