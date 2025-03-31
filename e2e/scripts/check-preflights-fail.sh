bccef2e751089608db946a662a85d7c29807e7ac
Andrew Lavery <laverya@umich.edu>
2025-03-31 15:36:05 -0500

    F

--- e2e/scripts/check-preflights-fail.sh
+++ e2e/scripts/check-preflights-fail.sh
@@ -12,7 +12,7 @@ main() {
         additional_args="${*:1}"
         echo "Running install with additional args: $additional_args"
     fi
-    if embedded-cluster install run-preflights --license /assets/license.yaml $additional_args 2>&1 ; then
+    if embedded-cluster install run-preflights --license /assets/license.yaml --yes $additional_args 2>&1 ; then
         echo "preflight_with_failure: Expected installation to fail"
         exit 1
     fi
