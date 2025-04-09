3c2c69b62b83885789303d79fbfecaa2b93c1ba7
Salah Aldeen Al Saleh <sg.alsaleh@gmail.com>
2025-04-09 14:01:16 -0700

    update admin console

--- e2e/playwright/tests/shared/deploy-app.ts
+++ e2e/playwright/tests/shared/deploy-app.ts
@@ -2,7 +2,7 @@ import { Page, Expect } from '@playwright/test';
 import { vaidateAppAndClusterReady } from '.';
 
 export const deployApp = async (page: Page, expect: Expect) => {
-  await expect(page.getByText('Optionally add nodes to the cluster'),).toBeVisible();
+  await expect(page.getByText('Add nodes to the cluster'),).toBeVisible();
   await expect(page.locator('.react-prism.language-bash')).toBeVisible();
   const joinCommand = await page.locator('.react-prism.language-bash').first().textContent();
   await expect(joinCommand).toContain('sudo');
