7c400a0930aa60a6dc24042637696a81096d5f3c
Alex Parker <7272359+ajp-io@users.noreply.github.com>
2025-04-10 11:07:28 -0400

    refresh output for install, update, and sb

--- e2e/scripts/single-node-airgap-install.sh
+++ e2e/scripts/single-node-airgap-install.sh
@@ -42,7 +42,7 @@ main() {
         kubectl get storageclass -A
         exit 1
     fi
-    if ! grep -q "Admin Console is ready!" /tmp/log; then
+    if ! grep -q "Admin Console is ready" /tmp/log; then
         echo "Failed to validate that the Admin Console is ready"
         exit 1
     fi
