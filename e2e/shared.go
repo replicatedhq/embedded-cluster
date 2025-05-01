9820f57083366a9e942009cf268b3cbbf9a247a3
Salah Aldeen Al Saleh <sg.alsaleh@gmail.com>
2025-05-01 08:00:10 -0700

    f

--- e2e/shared.go
+++ e2e/shared.go
@@ -200,7 +200,6 @@ func joinWorkerNodeWithOptions(t *testing.T, tc cluster.Cluster, node int, opts 
 
 	t.Logf("%s: joining node %d to the cluster as a worker", time.Now().Format(time.RFC3339), node)
 	for _, command := range commands {
-		t.Logf("running join command: %s: %t", command, strings.TrimSpace(command) == command)
 		if stdout, stderr, err := tc.RunCommandOnNode(node, strings.Fields(command), opts.withEnv); err != nil {
 			t.Fatalf("fail to join node %d to the cluster as a worker: %v: %s: %s", node, err, stdout, stderr)
 		}
