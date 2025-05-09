09f769e5c9d01d6b78850562040bf45b2c0de7ed
Salah Aldeen Al Saleh <sg.alsaleh@gmail.com>
2025-05-09 09:26:18 -0700

    f

--- scripts/ci-build-deps.sh
+++ scripts/ci-build-deps.sh
@@ -17,22 +17,29 @@ function init_vars() {
 }
 
 function local_artifact_mirror() {
-    make -C local-artifact-mirror build-ttl.sh
+    make -C local-artifact-mirror build-ttl.sh 2>&1 | prefix_output "LAM"
     cp local-artifact-mirror/build/image "local-artifact-mirror/build/image-$EC_VERSION"
 }
 
 function operator() {
     make -C operator build-ttl.sh build-chart-ttl.sh \
         PACKAGE_VERSION="$EC_VERSION" \
-        VERSION="$EC_VERSION"
+        VERSION="$EC_VERSION" 2>&1 | prefix_output "OPERATOR"
     cp operator/build/image "operator/build/image-$EC_VERSION"
     cp operator/build/chart "operator/build/chart-$EC_VERSION"
 }
 
 function main() {
     init_vars
-    local_artifact_mirror
-    operator
+    
+    local_artifact_mirror &
+    lam_pid=$!
+    
+    operator &
+    operator_pid=$!
+    
+    wait $lam_pid
+    wait $operator_pid
 }
 
 main "$@"
