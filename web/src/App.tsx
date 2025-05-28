3e168e85f29944cd9a15386a13f7be2a76da4975
Mia Wong <miawongdev@gmail.com>
2025-05-28 11:58:16 -0400

    remove unused

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,8 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { ConfigProvider } from './contexts/ConfigContext';
-import { WizardModeProvider } from './contexts/WizardModeContext';
-import { BrandingProvider } from './contexts/BrandingContext';
-import InstallWizard from './components/wizard/InstallWizard';
-import PrototypeSettings from './components/prototype/PrototypeSettings';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { ConfigProvider } from "./contexts/ConfigContext";
+import { WizardModeProvider } from "./contexts/WizardModeContext";
+import { BrandingProvider } from "./contexts/BrandingContext";
+import InstallWizard from "./components/wizard/InstallWizard";
 
 function App() {
   return (
@@ -12,23 +11,15 @@ function App() {
         <div className="min-h-screen bg-gray-50 text-gray-900 font-sans">
           <BrowserRouter>
             <Routes>
-              <Route path="/prototype" element={<PrototypeSettings />} />
-              <Route 
-                path="/" 
+              <Route
+                path="/"
                 element={
                   <WizardModeProvider mode="install">
                     <InstallWizard />
                   </WizardModeProvider>
-                } 
+                }
               />
-              <Route 
-                path="/upgrade" 
-                element={
-                  <WizardModeProvider mode="upgrade">
-                    <InstallWizard />
-                  </WizardModeProvider>
-                } 
-              />
+
               <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
           </BrowserRouter>
