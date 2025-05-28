d2d756a2f9579214eb592738a66be9e3354a3aae
Ethan Mosbaugh <ethan@replicated.com>
2025-05-28 05:39:30 -0700

    f

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: path.resolve(__dirname, './README.md'),
+          dest: './',
+        },
+      ],
+    }),
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
