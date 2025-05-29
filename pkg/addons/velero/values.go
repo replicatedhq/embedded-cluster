1e4c409e2af3cdc406db241b817c8f8b50b4f65f
Ethan Mosbaugh <ethan@replicated.com>
2025-05-29 12:39:56 -0700

    f

--- pkg/addons/velero/values.go
+++ pkg/addons/velero/values.go
@@ -74,6 +74,11 @@ func (v *Velero) GenerateHelmValues(ctx context.Context, kcli client.Client, ove
 	copiedValues["extraVolumes"] = extraVolumes
 	copiedValues["extraVolumeMounts"] = extraVolumeMounts
 
+	copiedValues["nodeAgent"] = map[string]any{
+		"extraVolumes":      extraVolumes,
+		"extraVolumeMounts": extraVolumeMounts,
+	}
+
 	podVolumePath := filepath.Join(v.EmbeddedClusterK0sSubDir, "kubelet/pods")
 	err = helm.SetValue(copiedValues, "nodeAgent.podVolumePath", podVolumePath)
 	if err != nil {
