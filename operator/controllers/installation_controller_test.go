9f3119d3e17b3ca92c61d19349f142ca6740962f
Ethan Mosbaugh <ethan@replicated.com>
2025-07-11 11:11:33 -0700

    f

--- operator/controllers/installation_controller_test.go
+++ operator/controllers/installation_controller_test.go
@@ -11,13 +11,11 @@ import (
 
 	"github.com/go-logr/logr"
 	"github.com/go-logr/logr/testr"
-	"github.com/replicatedhq/embedded-cluster/kinds/apis/v1beta1"
 	ecv1beta1 "github.com/replicatedhq/embedded-cluster/kinds/apis/v1beta1"
 	"github.com/replicatedhq/embedded-cluster/pkg/runtimeconfig"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
-	v1 "k8s.io/api/core/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -34,8 +32,8 @@ func TestInstallationReconciler_constructCreateCMCommand(t *testing.T) {
 		ObjectMeta: metav1.ObjectMeta{
 			Name: "install-name",
 		},
-		Spec: v1beta1.InstallationSpec{
-			RuntimeConfig: &v1beta1.RuntimeConfigSpec{
+		Spec: ecv1beta1.InstallationSpec{
+			RuntimeConfig: &ecv1beta1.RuntimeConfigSpec{
 				DataDir: "/var/lib/embedded-cluster",
 			},
 		},
@@ -54,11 +52,11 @@ func TestInstallationReconciler_constructCreateCMCommand(t *testing.T) {
 	expected := "if [ -f /embedded-cluster/support/host-preflight-results.json ]; then /embedded-cluster/bin/kubectl create configmap ${HSPF_CM_NAME} --from-file=results.json=/embedded-cluster/support/host-preflight-results.json -n embedded-cluster --dry-run=client -oyaml | /embedded-cluster/bin/kubectl label -f - embedded-cluster/host-preflight-result=${EC_NODE_NAME} --local -o yaml | /embedded-cluster/bin/kubectl apply -f - && /embedded-cluster/bin/kubectl annotate configmap ${HSPF_CM_NAME} \"update-timestamp=$(date +'%Y-%m-%dT%H:%M:%SZ')\" --overwrite; else echo '/embedded-cluster/support/host-preflight-results.json does not exist'; fi"
 	assert.Equal(t, expected, kctlCmd)
 	require.Len(t, job.Spec.Template.Spec.Containers[0].Env, 3)
-	assert.Equal(t, v1.EnvVar{
+	assert.Equal(t, corev1.EnvVar{
 		Name:  "EC_NODE_NAME",
 		Value: "my-node",
 	}, job.Spec.Template.Spec.Containers[0].Env[1])
-	assert.Equal(t, v1.EnvVar{
+	assert.Equal(t, corev1.EnvVar{
 		Name:  "HSPF_CM_NAME",
 		Value: "my-node-host-preflight-results",
 	}, job.Spec.Template.Spec.Containers[0].Env[2])
