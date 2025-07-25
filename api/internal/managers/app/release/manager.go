25834b85bb4cefccff251c3731646ac864588e7f
Salah Aldeen Al Saleh <sg.alsaleh@gmail.com>
2025-07-25 12:12:53 -0700

    Generate Helm values for each chart using its HelmChart custom resource

--- api/internal/managers/app/release/manager.go
+++ api/internal/managers/app/release/manager.go
@@ -15,6 +15,7 @@ import (
 // AppReleaseManager provides methods for managing the release of an app
 type AppReleaseManager interface {
 	TemplateHelmChartCRs(ctx context.Context, configValues types.AppConfigValues) ([]*kotsv1beta2.HelmChart, error)
+	GenerateHelmValues(ctx context.Context, templatedCR *kotsv1beta2.HelmChart) (map[string]any, error)
 }
 
 type appReleaseManager struct {
