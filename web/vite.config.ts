7a5df90842d0b2a5cf171c01b72598470a0a6847
João Antunes <joao@replicated.com>
2025-08-12 12:38:31 +0100

    chore: remove extra `/n`

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -6,9 +6,6 @@ import path from 'path';
 import { env } from 'process';
 import { InitialState } from './src/types';
 
-
-
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
