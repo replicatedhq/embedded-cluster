776d9a4b4a34d3b7ed7a1bd5c503c265fc11e3f8
Diamon Wiggins <diamonw757@gmail.com>
2025-08-12 16:33:19 -0400

    update comment

--- api/pkg/template/ca.go
+++ api/pkg/template/ca.go
@@ -5,10 +5,6 @@ import "github.com/replicatedhq/embedded-cluster/pkg/addons/adminconsole"
 // privateCACert returns the name of a ConfigMap containing additional CA certificates
 // provided by the host system. In Embedded Cluster, this ConfigMap is automatically
 // created and managed when a host CA bundle is detected.
-//
-// This function returns the constant ConfigMap name since Embedded Cluster
-// automatically manages the ConfigMap lifecycle. Future enhancements could check
-// for actual ConfigMap existence or host CA bundle availability.
 func (e *Engine) privateCACert() string {
 	return adminconsole.PrivateCASConfigMapName
 }
