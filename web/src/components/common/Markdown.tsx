fdfc508cad14a2b0400b641c9ffaae37f2990292
JGAntunes <joao@replicated.com>
2025-08-28 18:10:18 +0100

    chore: typo

--- web/src/components/common/Markdown.tsx
+++ web/src/components/common/Markdown.tsx
@@ -23,7 +23,7 @@ const Markdown: React.FC<MarkdownProps> = ({ children, rehypePlugins = [] }) => 
           />
         ),
         // We use:
-        // - pre selectors to style code blocks and inline code blocks differently. This is becasuse code blocks are rendered with a <pre><code>...</code></pre> structure
+        // - pre selectors to style code blocks and inline code blocks differently. This is because code blocks are rendered with a <pre><code>...</code></pre> structure
         // - before:content-none and after:content-none to remove the default backticks that are added by the Label component through the prose class
         code: ({ children }) => (
           <code className="font-mono text-xs bg-gray-100 px-1 py-0.5 rounded [pre_&]:text-sm [pre_&]:bg-transparent [pre_&]:px-0 [pre_&]:py-0 before:content-none after:content-none">
