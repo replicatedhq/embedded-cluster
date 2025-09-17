86c16d75f77d17617243446309f4d0d27ce3efdf
Ethan Mosbaugh <ethan@replicated.com>
2025-09-17 09:41:40 -0700

    fix(openebs): delete leftover pre-upgrade hook resources before upgrade

--- pkg/addons/openebs/upgrade.go
+++ pkg/addons/openebs/upgrade.go
@@ -8,6 +8,10 @@ import (
 	"github.com/replicatedhq/embedded-cluster/pkg/addons/types"
 	"github.com/replicatedhq/embedded-cluster/pkg/helm"
 	"github.com/sirupsen/logrus"
+	batchv1 "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/metadata"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -17,6 +21,11 @@ func (o *OpenEBS) Upgrade(
 	kcli client.Client, mcli metadata.Interface, hcli helm.Client,
 	domains ecv1beta1.Domains, overrides []string,
 ) error {
+	err := o.ensurePreUpgradeHooksDeleted(ctx, kcli)
+	if err != nil {
+		return errors.Wrap(err, "ensure pre-upgrade hooks deleted")
+	}
+
 	exists, err := hcli.ReleaseExists(ctx, o.Namespace(), o.ReleaseName())
 	if err != nil {
 		return errors.Wrap(err, "check if release exists")
@@ -44,6 +53,54 @@ func (o *OpenEBS) Upgrade(
 	})
 	if err != nil {
 		return errors.Wrap(err, "helm upgrade")
+	}
+
+	return nil
+}
+
+// ensurePostUpgradeHooksDeleted will delete helm hooks if for some reason they fail. It is
+// necessary if the hook does not have the "before-hook-creation" delete policy and instead has the
+// policy "hook-succeeded".
+func (a *OpenEBS) ensurePreUpgradeHooksDeleted(ctx context.Context, kcli client.Client) error {
+	job := &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "openebs-pre-upgrade-hook",
+		},
+	}
+	err := kcli.Delete(ctx, job, client.PropagationPolicy(metav1.DeletePropagationBackground))
+	if client.IgnoreNotFound(err) != nil {
+		return errors.Wrap(err, "delete openebs-pre-upgrade-hook job")
+	}
+
+	crb := &rbacv1.ClusterRoleBinding{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "openebs-pre-upgrade-hook",
+		},
+	}
+	err = kcli.Delete(ctx, crb)
+	if client.IgnoreNotFound(err) != nil {
+		return errors.Wrap(err, "delete openebs-pre-upgrade-hook cluster role binding")
+	}
+
+	cr := &rbacv1.ClusterRole{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "openebs-pre-upgrade-hook",
+		},
+	}
+	err = kcli.Delete(ctx, cr)
+	if client.IgnoreNotFound(err) != nil {
+		return errors.Wrap(err, "delete openebs-pre-upgrade-hook cluster role")
+	}
+
+	sa := &corev1.ServiceAccount{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: a.Namespace(),
+			Name:      "openebs-pre-upgrade-hook",
+		},
+	}
+	err = kcli.Delete(ctx, sa)
+	if client.IgnoreNotFound(err) != nil {
+		return errors.Wrap(err, "delete openebs-pre-upgrade-hook service account")
 	}
 
 	return nil
