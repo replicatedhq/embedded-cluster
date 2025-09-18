b7afe663a3f6693b29bdce2562d54f242082220e
João Antunes <joao@replicated.com>
2025-09-18 14:50:14 +0100

    fix(web): styling issues for typography in v3 (#2941)
    * fix(web): styling issues for typography in v3
    
    * fix: strategy class for forms plugin import

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,18 +1,4 @@
-import plugin from "tailwindcss/plugin";
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  corePlugins: {
-    preflight: true
-  },
-  plugins: [
-    plugin(function ({ addVariant }) {
-      addVariant("is-enabled", "&:not([disabled])");
-      addVariant("is-disabled", "&[disabled]");
-    }),
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    require("@tailwindcss/forms")({ strategy: "class" }),
-    require("@tailwindcss/typography")
-  ]
 };
