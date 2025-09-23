b6aa87ea6cfcdf1bb89533a1a05d4474e1adf496
Ethan Mosbaugh <ethan@replicated.com>
2025-09-23 15:23:02 -0700

    f

--- versions.mk
+++ versions.mk
@@ -33,13 +33,10 @@ KOTS_BINARY_URL_OVERRIDE =
 # For dev env, build the kots binary in the kots repo with "make kots-linux-arm64" and set this to "../kots/bin/kots"
 KOTS_BINARY_FILE_OVERRIDE =
 
-# K0S go version overrides go here
+# k0s version overrides go here
 
-# K0S binary source overrides go here
-K0S_BINARY_SOURCE_OVERRIDE =
 ifeq ($(K0S_VERSION),v1.31.12+k0s.0)
 K0S_VERSION = v1.31.12+k0s.0-ec.0
-K0S_BINARY_SOURCE_OVERRIDE = https://tf-staging-embedded-cluster-bin.s3.amazonaws.com/custom-k0s-binaries/k0s-v1.31.12%2Bk0s.0-ec.0-$(ARCH)
 endif
 
 # Require a new build be released if the patched k0s version changes
@@ -53,3 +50,11 @@ check-k0s-version:
 			exit 1; \
 		fi; \
 	fi
+
+# K0S go version overrides go here
+
+# K0S binary source overrides go here
+K0S_BINARY_SOURCE_OVERRIDE =
+ifeq ($(K0S_VERSION),v1.31.12+k0s.0-ec.0)
+K0S_BINARY_SOURCE_OVERRIDE = https://tf-staging-embedded-cluster-bin.s3.amazonaws.com/custom-k0s-binaries/k0s-v1.31.12%2Bk0s.0-ec.0-$(ARCH)
+endif
