78bcf099d9988640e2fcceca8b65786412727f0f
Ethan Mosbaugh <ethan@replicated.com>
2025-09-23 16:31:56 -0700

    chore: patch k0s 1.31 (#2969)
    * chore: patch k0s 1.31
    
    * f
    
    * f
    
    * f
    
    * f
    
    * f
    
    * f
    
    * f

--- versions.mk
+++ versions.mk
@@ -14,16 +14,7 @@ K0S_VERSION_1_29 = v1.29.15+k0s.0
 
 # Dynamic version selection
 K0S_VERSION = $(K0S_VERSION_1_$(K0S_MINOR_VERSION))
-
-# K0S Go Version Overrides (optional)
-# These allow overriding the Go version used for specific K0S minor versions
-# Format: K0S_GO_VERSION_OVERRIDE_<MINOR_VERSION>
-# Example: K0S_GO_VERSION_OVERRIDE_32 = v1.32.7+k0s.0.ec.1
-
-# K0S Binary Source Overrides (optional)
-# These allow overriding the binary source used for specific K0S minor versions
-# Format: K0S_BINARY_SOURCE_OVERRIDE_<MINOR_VERSION>
-# Example: K0S_BINARY_SOURCE_OVERRIDE_32 = https://github.com/k0sproject/k0s/releases/download/v1.32.7+k0s.0/k0s-v1.32.7+k0s.0-amd64
+K0S_GO_VERSION = $(K0S_VERSION_1_$(K0S_MINOR_VERSION))
 
 # Troubleshoot Version
 TROUBLESHOOT_VERSION = v0.122.0
@@ -41,3 +32,29 @@ KOTS_BINARY_URL_OVERRIDE =
 # If KOTS_BINARY_FILE_OVERRIDE is set, KOTS_VERSION will be dynamically generated
 # For dev env, build the kots binary in the kots repo with "make kots-linux-arm64" and set this to "../kots/bin/kots"
 KOTS_BINARY_FILE_OVERRIDE =
+
+# k0s version overrides go here
+
+ifeq ($(K0S_VERSION),v1.31.12+k0s.0)
+K0S_VERSION = v1.31.12+k0s.0-ec.0
+endif
+
+# K0S go version overrides go here
+
+# K0S binary source overrides go here
+K0S_BINARY_SOURCE_OVERRIDE =
+ifeq ($(K0S_VERSION),v1.31.12+k0s.0-ec.0)
+K0S_BINARY_SOURCE_OVERRIDE = https://tf-staging-embedded-cluster-bin.s3.amazonaws.com/custom-k0s-binaries/k0s-v1.31.12%2Bk0s.0-ec.0-$(ARCH)
+endif
+
+# Require a new build be released if the patched k0s version changes
+.PHONY: check-k0s-version
+check-k0s-version:
+	@if echo "$(K0S_VERSION)" | grep -q "^v1\.31"; then \
+		if [ "$(K0S_VERSION)" != "v1.31.12+k0s.0-ec.0" ]; then \
+			echo "Error: K0S_VERSION starts with v1.31 but does not equal v1.31.12+k0s.0-ec.0"; \
+			echo "Current K0S_VERSION: $(K0S_VERSION)"; \
+			echo "Expected: v1.31.12+k0s.0-ec.0"; \
+			exit 1; \
+		fi; \
+	fi
