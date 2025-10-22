2deb45dbb02ce322fbf465cb93b637569ac2db6a
Steven Crespo <screswk@gmail.com>
2025-10-22 15:02:05 -0700

    f
    Signed-off-by: Steven Crespo <screswk@gmail.com>

--- api/internal/managers/kubernetes/infra/install.go
+++ api/internal/managers/kubernetes/infra/install.go
@@ -135,7 +135,10 @@ func (m *infraManager) installAddOns(ctx context.Context, kcli client.Client, mc
 		addons.WithProgressChannel(progressChan),
 	)
 
-	opts := m.getAddonInstallOpts(ctx, license, ki)
+	opts, err := m.getAddonInstallOpts(ctx, license, ki)
+	if err != nil {
+		return fmt.Errorf("get addon install options: %w", err)
+	}
 
 	logFn("installing addons")
 	if err := addOns.InstallKubernetes(ctx, opts); err != nil {
@@ -145,10 +148,10 @@ func (m *infraManager) installAddOns(ctx context.Context, kcli client.Client, mc
 	return nil
 }
 
-func (m *infraManager) getAddonInstallOpts(ctx context.Context, license *kotsv1beta1.License, ki kubernetesinstallation.Installation) addons.KubernetesInstallOptions {
+func (m *infraManager) getAddonInstallOpts(ctx context.Context, license *kotsv1beta1.License, ki kubernetesinstallation.Installation) (addons.KubernetesInstallOptions, error) {
 	kotsadmNamespace, err := runtimeconfig.KotsadmNamespace(ctx, m.kcli)
 	if err != nil {
-		m.logger.WithError(err).Error("get kotsadm namespace")
+		return addons.KubernetesInstallOptions{}, fmt.Errorf("get kotsadm namespace: %w", err)
 	}
 	opts := addons.KubernetesInstallOptions{
 		AdminConsolePwd:    m.password,
@@ -167,5 +170,5 @@ func (m *infraManager) getAddonInstallOpts(ctx context.Context, license *kotsv1b
 
 	// TODO: no kots app install for now
 
-	return opts
+	return opts, nil
 }
