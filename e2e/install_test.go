cd7275132c30bf72db0caa29d6bfa3fe96953ab3
Josh De Winne <joris.dewinne@gmail.com>
2025-11-10 09:48:51 -0800

    Enable TestSingleNodeNetworkReport

--- e2e/install_test.go
+++ e2e/install_test.go
@@ -3,6 +3,7 @@ package e2e
 import (
 	"encoding/base64"
 	"fmt"
+	"net"
 	"os"
 	"strings"
 	"testing"
@@ -690,8 +691,8 @@ spec:
 	t.Logf("%s: test complete", time.Now().Format(time.RFC3339))
 }
 
-//Temporarily disable network test until the reporting is enriched to the point where we can properly filter out domains as part of a CNAME chain
-/*func TestSingleNodeNetworkReport(t *testing.T) {
+// Temporarily disable network test until the reporting is enriched to the point where we can properly filter out domains as part of a CNAME chain
+func TestSingleNodeNetworkReport(t *testing.T) {
 	t.Parallel()
 	RequireEnvVars(t, []string{"SHORT_SHA"})
 	tc := cmx.NewCluster(&cmx.ClusterInput{
@@ -773,4 +774,4 @@ spec:
 			t.Fail()
 		}
 	}
-}*/
+}
