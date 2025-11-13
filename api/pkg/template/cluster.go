3ab44cb6ae55f47057829af7ac7ddc44def70851
JGAntunes <joao@replicated.com>
2025-11-13 18:37:47 +0000

    chore: add logging

--- api/pkg/template/cluster.go
+++ api/pkg/template/cluster.go
@@ -9,12 +9,15 @@ import (
 // nodeCount returns the total count of Kubernetes nodes in the cluster.
 // It returns 0 if the kubeClient is unavailable or if an error occurs.
 func (e *Engine) nodeCount() int {
+	logger := e.logger.WithField("function", "node-count")
 	if e.kubeClient == nil {
+		logger.Warn("kube client is nil, cannot get node count")
 		return 0
 	}
 	ctx := context.TODO()
 	nodeList := &corev1.NodeList{}
 	if err := e.kubeClient.List(ctx, nodeList); err != nil {
+		logger.WithError(err).Warn("failed to list nodes")
 		return 0
 	}
 	return len(nodeList.Items)
