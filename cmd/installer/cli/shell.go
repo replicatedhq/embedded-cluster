8b30bbbb37fb2f602bc003eda44a9a7facd404ed
Ethan Mosbaugh <ethan@replicated.com>
2025-12-11 19:25:57 -0800

    f

--- cmd/installer/cli/shell.go
+++ cmd/installer/cli/shell.go
@@ -154,7 +154,11 @@ func openInteractiveShell(shpath string, rc runtimeconfig.RuntimeConfig) error {
 	_, _ = shellpty.WriteString(config)
 	_, _ = io.CopyN(io.Discard, shellpty, int64(len(config)+1))
 
-	config = "export TROUBLESHOOT_AUTO_UPDATE=false\nexport PREFLIGHT_AUTO_UPDATE=false\n"
+	config = "export TROUBLESHOOT_AUTO_UPDATE=false\n"
+	_, _ = shellpty.WriteString(config)
+	_, _ = io.CopyN(io.Discard, shellpty, int64(len(config)+1))
+
+	config = "export PREFLIGHT_AUTO_UPDATE=false\n"
 	_, _ = shellpty.WriteString(config)
 	_, _ = io.CopyN(io.Discard, shellpty, int64(len(config)+1))
 
