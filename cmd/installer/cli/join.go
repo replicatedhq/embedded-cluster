a318db8e81961453fec3d0bedbcff74c942184b2
Ethan Mosbaugh <ethan@replicated.com>
2026-05-12 06:00:15 -0700

    refactor(k0s,join): remove arbitrary 30s timeout around ApplyHostK0sConfigOverrides

--- cmd/installer/cli/join.go
+++ cmd/installer/cli/join.go
@@ -7,7 +7,6 @@ import (
 	"os"
 	"strings"
 	"syscall"
-	"time"
 
 	"github.com/AlecAivazis/survey/v2/terminal"
 	"github.com/replicatedhq/embedded-cluster/cmd/installer/goods"
@@ -506,9 +505,7 @@ func applyNetworkConfiguration(networkInterface string, rc runtimeconfig.Runtime
 		clusterSpec.Spec.API.ExtraArgs["service-node-port-range"] = rc.NodePortRange()
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-	defer cancel()
-	if err := config.ApplyHostK0sConfigOverrides(ctx, clusterSpec); err != nil {
+	if err := config.ApplyHostK0sConfigOverrides(context.TODO(), clusterSpec); err != nil {
 		return fmt.Errorf("apply host k0s config overrides: %w", err)
 	}
 
